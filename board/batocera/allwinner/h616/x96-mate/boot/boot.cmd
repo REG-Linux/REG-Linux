b938f2282e8620798a4ec36782d40d9704629f3e
Romain Tisserand <romain.tisserand@gmail.com>
2024-04-04 22:33:02 +0200

    label=REGLINUX for all targets

--- board/batocera/allwinner/h616/x96-mate/boot/boot.cmd
+++ board/batocera/allwinner/h616/x96-mate/boot/boot.cmd
@@ -3,7 +3,7 @@
 # mkimage -A arm64 -T script -O linux -d boot.cmd boot.scr
 #
 
-setenv bootargs initrd=/boot/initrd.lz4 label=BATOCERA rootwait earlycon loglevel=9 console=ttyS0,115200 console=tty3
+setenv bootargs initrd=/boot/initrd.lz4 label=REGLINUX rootwait earlycon loglevel=9 console=ttyS0,115200 console=tty3
 
 load ${devtype} ${devnum}:${bootpart} ${kernel_addr_r} /boot/linux
 load ${devtype} ${devnum}:${bootpart} ${fdt_addr_r} /boot/sun50i-h616-x96-mate.dtb
