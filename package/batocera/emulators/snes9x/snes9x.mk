79b978a8b6baf9c7c4daa73999b8e9cee9fde084
Demetris Ierokipides <ierokipides.dem@gmail.com>
2024-04-15 02:17:53 +0300

    Snes9x: update to the latest

--- package/batocera/emulators/snes9x/snes9x.mk
+++ package/batocera/emulators/snes9x/snes9x.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SNES9X_VERSION = 1e1c45be07bf5760e73414d9ed0253d6dedb8605
+SNES9X_VERSION = 9d22dbb8d866f10042fe564db7dc7b41078a580b
 SNES9X_SITE = https://github.com/snes9xgit/snes9x.git
 SNES9X_SITE_METHOD = git
 SNES9X_GIT_SUBMODULES = YES
