eb92407ac79cf5281d059fb7329665425cb27f0e
Romain TISSERAND <romain.tisserand@gmail.com>
2024-04-18 12:21:19 +0200

    Fix building libopenmpt without PulseAudio

--- package/batocera/libraries/libopenmpt/libopenmpt.mk
+++ package/batocera/libraries/libopenmpt/libopenmpt.mk
@@ -11,4 +11,12 @@ LIBOPENMPT_INSTALL_STAGING = YES
 LIBOPENMPT_AUTORECONF = YES
 LIBOPENMPT_DEPENDENCIES = host-pkgconf zlib host-doxygen mpg123
 
+ifeq ($(BR2_PACKAGE_PULSEAUDIO),)
+define LIBOPENMPT_CONFIGURE_CMDS
+    cd $(@D); ./autogen.sh; $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE="$(HOST_DIR)/usr/bin/" \
+        ./configure --host="$(GNU_TARGET_NAME)" --without-pulseaudio --without-sndfile \
+                    --prefix=/usr
+endef
+endif
+
 $(eval $(autotools-package))
