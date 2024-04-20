7304b5737be52b9ffbfdd85bf48b78ee42ea9e27
Romain TISSERAND <romain.tisserand@gmail.com>
2024-04-20 17:31:37 +0200

    Make libretro-hatarib cross-compiling, enable on all targets

--- package/batocera/emulators/retroarch/libretro/libretro-hatarib/libretro-hatarib.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-hatarib/libretro-hatarib.mk
@@ -12,6 +12,8 @@ LIBRETRO_HATARIB_DEPENDENCIES = libcapsimage libpng libzlib sdl2
 
 LIBRETRO_HATARIB_GIT_SUBMODULES = YES
 
+LIBRETRO_HATARIB_CC_FLAGS = -fPIC -D__LIBRETRO__ -DSHORTHASH="\"reglinux\"" -I$(@D)/hatari -I$(@D)/hatari/build -I$(STAGING_DIR)/usr/include/SDL2 -L$(STAGING_DIR)/usr/lib -lSDL2 -I$(STAGING_DIR)/usr/include -lz
+
 LIBRETRO_HATARIB_CONF_ENV += \
     SHORTHASH='"$(shell echo $(LIBRETRO_HATARIB_VERSION) | cut -c 1-7)"' \
 	SDL2_INCLUDE="$(STAGING_DIR)/usr/include/SDL2" \
@@ -19,12 +21,14 @@ LIBRETRO_HATARIB_CONF_ENV += \
 	SDL2_LINK="$(STAGING_DIR)/usr/lib/libSDL2.so" \
 	ZLIB_INCLUDE="$(STAGING_DIR)/usr/include" \
 	ZLIB_LIB="$(STAGING_DIR)/usr/lib" \
-	ZLIB_LINK="$(STAGING_DIR)/usr/lib/libz.so"
+	ZLIB_LINK="$(STAGING_DIR)/usr/lib/libz.so" \
+	CFLAGS="$(TARGET_CFLAGS) $(LIBRETRO_HATARIB_CC_FLAGS)" \
+	CC="$(TARGET_CC)" \
+	AR="$(TARGET_AR)" \
+	RANLIB="$(TARGET_RANLIB)"
 
 define LIBRETRO_HATARIB_BUILD_CMDS
-    CC="$(TARGET_CC)" AR="$(TARGET_AR)" RANLIB="$(TARGET_RANLIB)" \
-	cd $(@D) && $(MAKE) -f makefile \
-	$(LIBRETRO_HATARIB_CONF_ENV)
+	cd $(@D) && $(MAKE) -f makefile $(LIBRETRO_HATARIB_CONF_ENV)
 endef
 
 define LIBRETRO_HATARIB_INSTALL_TARGET_CMDS
