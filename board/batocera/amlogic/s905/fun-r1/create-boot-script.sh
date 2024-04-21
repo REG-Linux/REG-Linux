2ec7630fb9e765ac339bf2e68f52897595fafb55
Romain TISSERAND <romain.tisserand@gmail.com>
2024-04-21 21:07:45 +0200

    Rename some references + speedup systemsReport.sh script

--- board/batocera/amlogic/s905/fun-r1/create-boot-script.sh
+++ board/batocera/amlogic/s905/fun-r1/create-boot-script.sh
@@ -5,7 +5,7 @@
 # BUILD_DIR = base dir/build
 # BINARIES_DIR = images dir
 # TARGET_DIR = target dir
-# BATOCERA_BINARIES_DIR = batocera binaries sub directory
+# REGLINUX_BINARIES_DIR = reglinux binaries sub directory
 # BATOCERA_TARGET_DIR = batocera target sub directory
 
 HOST_DIR=$1
@@ -13,27 +13,27 @@ BOARD_DIR=$2
 BUILD_DIR=$3
 BINARIES_DIR=$4
 TARGET_DIR=$5
-BATOCERA_BINARIES_DIR=$6
+REGLINUX_BINARIES_DIR=$6
 
-mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot" 	  || exit 1
-mkdir -p "${BATOCERA_BINARIES_DIR}/boot/extlinux" || exit 1
+mkdir -p "${REGLINUX_BINARIES_DIR}/boot/boot" 	  || exit 1
+mkdir -p "${REGLINUX_BINARIES_DIR}/boot/extlinux" || exit 1
 
 
-"${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n linux -d "${BINARIES_DIR}/Image" "${BATOCERA_BINARIES_DIR}/boot/boot/uImage" || exit 1
-cp "${BINARIES_DIR}/uInitrd"         "${BATOCERA_BINARIES_DIR}/boot/boot/uInitrd"         || exit 1
-cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
-cp "${BINARIES_DIR}/modules"         "${BATOCERA_BINARIES_DIR}/boot/boot/modules.update"  || exit 1
-cp "${BINARIES_DIR}/firmware"        "${BATOCERA_BINARIES_DIR}/boot/boot/firmware.update" || exit 1
-cp "${BINARIES_DIR}/rescue"          "${BATOCERA_BINARIES_DIR}/boot/boot/rescue.update"   || exit 1
+"${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n linux -d "${BINARIES_DIR}/Image" "${REGLINUX_BINARIES_DIR}/boot/boot/uImage" || exit 1
+cp "${BINARIES_DIR}/uInitrd"         "${REGLINUX_BINARIES_DIR}/boot/boot/uInitrd"         || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs" "${REGLINUX_BINARIES_DIR}/boot/boot/reglinux.update" || exit 1
+cp "${BINARIES_DIR}/modules"         "${REGLINUX_BINARIES_DIR}/boot/boot/modules.update"  || exit 1
+cp "${BINARIES_DIR}/firmware"        "${REGLINUX_BINARIES_DIR}/boot/boot/firmware.update" || exit 1
+cp "${BINARIES_DIR}/rescue"          "${REGLINUX_BINARIES_DIR}/boot/boot/rescue.update"   || exit 1
 
-cp "${BOARD_DIR}/boot/boot-logo.bmp.gz" 	"${BATOCERA_BINARIES_DIR}/boot/"	  || exit 1
-cp "${BOARD_DIR}/boot/README.txt"       	"${BATOCERA_BINARIES_DIR}/boot/"	  || exit 1
-cp "${BOARD_DIR}/boot/uEnv.txt"       		"${BATOCERA_BINARIES_DIR}/boot/" 	  || exit 1
+cp "${BOARD_DIR}/boot/boot-logo.bmp.gz" 	"${REGLINUX_BINARIES_DIR}/boot/"	  || exit 1
+cp "${BOARD_DIR}/boot/README.txt"       	"${REGLINUX_BINARIES_DIR}/boot/"	  || exit 1
+cp "${BOARD_DIR}/boot/uEnv.txt"       		"${REGLINUX_BINARIES_DIR}/boot/" 	  || exit 1
 
-cp "${BINARIES_DIR}/meson-gxl-s905x-fun-r1.dtb" "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
+cp "${BINARIES_DIR}/meson-gxl-s905x-fun-r1.dtb" "${REGLINUX_BINARIES_DIR}/boot/boot/"     || exit 1
 
-"${HOST_DIR}/bin/mkimage" -C none -A arm64 -T script -d "${BOARD_DIR}/boot/s905_autoscript.txt" "${BATOCERA_BINARIES_DIR}/boot/s905_autoscript" || exit 1
-"${HOST_DIR}/bin/mkimage" -C none -A arm64 -T script -d "${BOARD_DIR}/boot/aml_autoscript.txt"  "${BATOCERA_BINARIES_DIR}/boot/aml_autoscript"  || exit 1
-cp "${BOARD_DIR}/boot/aml_autoscript.zip" "${BATOCERA_BINARIES_DIR}/boot" || exit 1
+"${HOST_DIR}/bin/mkimage" -C none -A arm64 -T script -d "${BOARD_DIR}/boot/s905_autoscript.txt" "${REGLINUX_BINARIES_DIR}/boot/s905_autoscript" || exit 1
+"${HOST_DIR}/bin/mkimage" -C none -A arm64 -T script -d "${BOARD_DIR}/boot/aml_autoscript.txt"  "${REGLINUX_BINARIES_DIR}/boot/aml_autoscript"  || exit 1
+cp "${BOARD_DIR}/boot/aml_autoscript.zip" "${REGLINUX_BINARIES_DIR}/boot" || exit 1
 
 exit 0
