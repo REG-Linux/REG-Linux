f3a4d5412c166c1cb98c835517c0d82de2a25fb9
Romain TISSERAND <romain.tisserand@gmail.com>
2024-04-27 15:06:03 +0200

    Bump amiberry, fallout2-ce, cdogs (needs data), raze, mokutil, lime3ds, faudio, linux-firmware, lr-core-info

--- package/reglinux/wsdd-native/wsdd-native.mk
+++ package/reglinux/wsdd-native/wsdd-native.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WSDD_NATIVE_VERSION = 1.8
+WSDD_NATIVE_VERSION = 1.9
 WSDD_NATIVE_SITE = $(call github,gershnik,wsdd-native,v$(WSDD_NATIVE_VERSION))
 WSDD_NATIVE_LICENSE = BSD
 
@@ -13,7 +13,7 @@ WSDD_NATIVE_CONF_OPTS += -DWSDDN_PREFER_SYSTEM=OFF -DWSDDN_WITH_SYSTEMD="no" -DC
 
 define WSDD_NATIVE_INSTALL_TARGET_CMDS
         $(INSTALL) -Dm755 $(@D)/wsddn $(TARGET_DIR)/usr/bin/wsdd
-        $(INSTALL) -Dm755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/wsdd-native/S93wsdd $(TARGET_DIR)/etc/init.d
+        $(INSTALL) -Dm755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/reglinux/wsdd-native/S93wsdd $(TARGET_DIR)/etc/init.d
 endef
 
 $(eval $(cmake-package))
