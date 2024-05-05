2d15402750c0b9effee1ad484acf251473c5cf9d
Romain TISSERAND <romain.tisserand@gmail.com>
2024-05-05 13:54:46 +0200

    Switch BigPEmu to native (no Wine)

--- package/batocera/emulators/bigpemu/bigpemu.mk
+++ package/batocera/emulators/bigpemu/bigpemu.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-BIGPEMU_VERSION = v1094
-BIGPEMU_SOURCE = BigPEmu_$(BIGPEMU_VERSION).zip
+BIGPEMU_VERSION = Linux64_v112
+BIGPEMU_SOURCE = BigPEmu_$(BIGPEMU_VERSION).tar.gz
 BIGPEMU_SITE = https://www.richwhitehouse.com/jaguar/builds
 
 define BIGPEMU_EXTRACT_CMDS
-	mkdir -p $(@D) && cd $(@D) && unzip -x $(DL_DIR)/$(BIGPEMU_DL_SUBDIR)/$(BIGPEMU_SOURCE)
+	mkdir -p $(@D) && cd $(@D) && tar xzvf $(DL_DIR)/$(BIGPEMU_DL_SUBDIR)/$(BIGPEMU_SOURCE) --strip-components=1
 endef
 
 define BIGPEMU_INSTALL_TARGET_CMDS
