f8ad45c9f061903cf2e63a4849780615f6eddc7b
Romain TISSERAND <romain.tisserand@gmail.com>
2024-05-11 00:26:50 +0200

    First draft

--- package/batocera/controllers/qtsixa-shanwan/qtsixa-shanwan.mk
+++ package/batocera/controllers/qtsixa-shanwan/qtsixa-shanwan.mk
@@ -12,13 +12,6 @@ QTSIXA_SHANWAN_INCLUDES =-I$(STAGING_DIR)/usr/include
 QTSIXA_SHANWAN_CFLAGS = -D__ARM_PCS_VFP -DARM_ARCH -Wall $(QTSIXA_SHANWAN_INCLUDES)
 QTSIXA_SHANWAN_LIBS = -ldl -lpthread -lz -L$(STAGING_DIR)/usr/lib -lrt -lusb -lbluetooth
 
-ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
-	QTSIXA_INCLUDES +=-I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
-	QTSIXA_CFLAGS += -DRPI_BUILD
-	QTSIXA_LIBS += -lbcm_host -lvcos -lvchiq_arm -lvchostif
-endif
-
-
 define QTSIXA_SHANWAN_BUILD_CMDS
 	# Make SHANWAN
 	$(SED) "s|/usr/.\+\?/sixad-remote|/usr/sixad/shanwan/sixad-remote|g" $(@D)/sixad/bluetooth.cpp
@@ -30,7 +23,7 @@ define QTSIXA_SHANWAN_BUILD_CMDS
 endef
 
 define QTSIXA_SHANWAN_INSTALL_TARGET_CMDS
-	$(MAKE) INSTALLDIR="shanwan" BINDIR="shanwan" DESTDIR=$(TARGET_DIR) -C $(@D)/sixad install 
+	$(MAKE) INSTALLDIR="shanwan" BINDIR="shanwan" DESTDIR=$(TARGET_DIR) -C $(@D)/sixad install
 endef
 
 define QTSIXA_SHANWAN_RPI_FIXUP
