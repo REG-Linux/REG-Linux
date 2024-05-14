080763c9ba460b54ebc7c35d168a507b9baeaf3a
Romain TISSERAND <romain.tisserand@gmail.com>
2024-05-14 02:37:07 +0200

    Move disabled batocera ports to reglinux namespace where they are enabled

--- package/reglinux/ports/openjazz/openjazz.mk
+++ package/reglinux/ports/openjazz/openjazz.mk
@@ -9,21 +9,14 @@ OPENJAZZ_SITE =  $(call github,AlisterT,openjazz,$(OPENJAZZ_VERSION))
 OPENJAZZ_DEPENDENCIES = sdl2
 OPENJAZZ_LICENSE = GPLv2
 
-#define OPENJAZZ_BUILD_CMDS
-#		$(TARGET_CONFIGURE_OPTS) $(MAKE) \
-#		CPP="$(TARGET_CPP)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
-#		AS="$(TARGET_CC)" LD="$(TARGET_LD)" STRIP="$(TARGET_STRIP)" \
-#		-C $(@D) -f Makefile
-#endef
-
 define OPENJAZZ_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/OpenJazz $(TARGET_DIR)/usr/bin/OpenJazz
 endef
 
 define OPENJAZZ_POST_PROCESS
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/openjazz/openjazz.keys $(TARGET_DIR)/usr/share/evmapy
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/openjazz/openjazz.cfg $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/openjazz.cfg
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/reglinux/ports/openjazz/openjazz.keys $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/reglinux/ports/openjazz/openjazz.cfg $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/openjazz.cfg
 endef
 
 $(eval $(cmake-package))
