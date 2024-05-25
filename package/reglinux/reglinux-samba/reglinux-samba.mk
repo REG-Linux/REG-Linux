eab8371e95a4e8b446f4a9144ac1c9ebac0a71a0
Romain TISSERAND <romain.tisserand@gmail.com>
2024-05-26 01:17:03 +0200

    Fix S91smb / S91ksmbd clash by rewriting reglinux-samba properly

--- package/reglinux/reglinux-samba/reglinux-samba.mk
+++ package/reglinux/reglinux-samba/reglinux-samba.mk
@@ -4,27 +4,32 @@
 #
 ################################################################################
 
-REGLINUX_SAMBA_VERSION = 0.3
+REGLINUX_SAMBA_VERSION = 0.4
 REGLINUX_SAMBA_SOURCE =
-
 REGLINUX_SAMBA_DEPENDENCIES =
-
 
 # Install S91ksmbd service
 define REGLINUX_SAMBA_INSTALL_KSMBD_INIT
-	# Remove S91smb service
-	if test -f "$(TARGET_DIR)/etc/init.d/S91smb"; then rm $(TARGET_DIR)/etc/init.d/S91smb ; fi
-	# Install S91ksmbd instead
-	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/reglinux/reglinux-samba/S91ksmbd $(TARGET_DIR)/etc/init.d/S91ksmbd
+	# Install S91ksmbd service
+	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/reglinux/reglinux-samba/S91ksmbd $(TARGET_DIR)/etc/init.d/S91smbd
 endef
 
-# We need samba4 for Wine x86, and old kernels
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_ANY)$(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_4_4)$(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_5_4)$(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_5_10),y)
+# Install S91smbd service
+define REGLINUX_SAMBA_INSTALL_SMBD_INIT
+	# Install S91smbd service
+	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/reglinux/reglinux-samba/S91smbd $(TARGET_DIR)/etc/init.d/S91smbd
+endef
+
+# We need samba4 on x86 for Wine
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_ANY),y)
 REGLINUX_SAMBA_DEPENDENCIES += samba4
 endif
 
-# Everything but those archs will run ksmbd
-ifneq ($(BR2_PACKAGE_BATOCERA_TARGET_RK3128)$(BR2_PACKAGE_BATOCERA_TARGET_A3GEN2)$(BR2_PACKAGE_BATOCERA_TARGET_S9GEN4)$(BR2_PACKAGE_BATOCERA_TARGET_RK3588)$(BR2_PACKAGE_BATOCERA_TARGET_X86_ANY),y)
+# We use samba4 as SMB service for old kernels (< 5.15), if notyou run ksmbd
+ifeq ($(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_4_4)$(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_5_4)$(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_5_10),y)
+REGLINUX_SAMBA_DEPENDENCIES += samba4
+REGLINUX_SAMBA_POST_INSTALL_TARGET_HOOKS += REGLINUX_SAMBA_INSTALL_SMBD_INIT
+else
 REGLINUX_SAMBA_DEPENDENCIES += ksmbd-tools
 REGLINUX_SAMBA_POST_INSTALL_TARGET_HOOKS += REGLINUX_SAMBA_INSTALL_KSMBD_INIT
 endif
