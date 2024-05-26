786a442361a0d4f8940d875ab5f946c80a52e0f3
Juliano Dorigão <jdorigao@gmail.com>
2024-05-25 22:08:21 -0400

    joycond: S97joycond moved to package

--- package/batocera/controllers/joycond/joycond.mk
+++ package/batocera/controllers/joycond/joycond.mk
@@ -9,12 +9,13 @@ JOYCOND_SITE = $(call github,DanielOgorchock,joycond,$(JOYCOND_VERSION))
 JOYCOND_LICENSE = GPL-3.0+
 JOYCOND_LICENSE_FILES = LICENSE
 JOYCOND_DEPENDENCIES = acl libevdev udev
+JOYCOND_PATH = $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/joycond
 
 define JOYCOND_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/joycond $(TARGET_DIR)/usr/bin
-    $(INSTALL) -D -m 0644 $(@D)/udev/*.rules $(TARGET_DIR)/etc/udev/rules.d/
-    $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/joycond/99-joycond-ignore.rules \
-        $(TARGET_DIR)/etc/udev/rules.d/
+    $(INSTALL) -D -m 0755 $(@D)/joycond						$(TARGET_DIR)/usr/bin
+    $(INSTALL) -D -m 0755 $(JOYCOND_PATH)/config/S97joycond			$(TARGET_DIR)/etc/init.d/
+    $(INSTALL) -D -m 0644 $(@D)/udev/*.rules					$(TARGET_DIR)/etc/udev/rules.d/
+    $(INSTALL) -D -m 0644 $(JOYCOND_PATH)/config/99-joycond-ignore.rules	$(TARGET_DIR)/etc/udev/rules.d/
 endef
 
 $(eval $(cmake-package))
