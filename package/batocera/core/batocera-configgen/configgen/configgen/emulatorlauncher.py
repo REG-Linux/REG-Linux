b025f84b5b69f82a749200f3244e3be50ad3ca79
Romain TISSERAND <romain.tisserand@gmail.com>
2024-05-31 19:21:18 +0200

    Fix launching games if mangohud is not present, need to update ES to handle that too...

--- package/batocera/core/batocera-configgen/configgen/configgen/emulatorlauncher.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/emulatorlauncher.py
@@ -334,7 +334,7 @@ def start_rom(args, maxnbplayers, rom, romConfiguration):
 
             cmd = generator.generate(system, rom, playersControllers, metadata, guns, wheels, gameResolution)
 
-            if system.isOptSet('hud_support') and system.getOptBoolean('hud_support') == True:
+            if system.isOptSet('hud_support') and os.path.exists("/usr/bin/mangohud") and system.getOptBoolean('hud_support') == True:
                 hud_bezel = getHudBezel(system, generator, rom, gameResolution, controllers.gunsBordersSizeName(guns, system.config))
                 if (system.isOptSet('hud') and system.config['hud'] != "" and system.config['hud'] != "none") or hud_bezel is not None:
                     gameinfos = extractGameInfosFromXml(args.gameinfoxml)
