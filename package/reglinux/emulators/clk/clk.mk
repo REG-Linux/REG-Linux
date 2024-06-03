01a72814f1cf9963689156b1de617aa9372dc555
Romain TISSERAND <romain.tisserand@gmail.com>
2024-06-03 23:40:56 +0200

    Add CLK (Clock Signal) emulator (requires Desktop GL)

---
+++ package/reglinux/emulators/clk/clk.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# CLK - Clock Signal emulator
+#
+################################################################################
+# Version.: Release on Jun 3, 2024
+CLK_VERSION = 2024-06-03
+CLK_SITE = https://github.com/TomHarte/CLK
+CLK_SITE_METHOD=git
+CLK_LICENSE = GPLv3
+CLK_DEPENDENCIES = sdl2 libgl zlib
+
+CLK_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+
+$(eval $(cmake-package))
