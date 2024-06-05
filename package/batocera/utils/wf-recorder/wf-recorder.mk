57e414f19fb2eecbdc3cd5cc95e7507e9f434a05
Romain TISSERAND <romain.tisserand@gmail.com>
2024-06-05 20:11:18 +0200

    Misc fixes (don't enforce pulseaudio, allow sonic3 air on plain SDL2/GLES2 + misc fixes)

--- package/batocera/utils/wf-recorder/wf-recorder.mk
+++ package/batocera/utils/wf-recorder/wf-recorder.mk
@@ -8,8 +8,12 @@ WF_RECORDER_VERSION = v0.4.1
 WF_RECORDER_SITE = $(call github,ammen99,wf-recorder,$(WF_RECORDER_VERSION))
 WF_RECORDER_LICENSE = MIT
 WF_RECORDER_LICENSE_FILES = LICENSE
-WF_RECORDER_DEPENDENCIES = ffmpeg pulseaudio
+WF_RECORDER_DEPENDENCIES = ffmpeg
 
+ifeq ($(BR2_PACKAGE_PULSEAUDIO),y)
 WF_RECORDER_CONF_OPTS = -Dpulse=enabled
+else
+WF_RECORDER_CONF_OPTS = -Dpulse=disabled
+endif
 
 $(eval $(meson-package))
