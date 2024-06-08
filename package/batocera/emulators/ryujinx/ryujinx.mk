2021b2d8cfb87678a856a1945060a24baf856eed
Romain TISSERAND <romain.tisserand@gmail.com>
2024-06-08 02:04:39 +0200

    Misc fixes: - Bump REG-Rescue to 0.4 - Remove vim - Fix clksignal missing install - Fix ryujinx "Ava" binary (same hash as main one) - Disable VCMI for now

--- package/batocera/emulators/ryujinx/ryujinx.mk
+++ package/batocera/emulators/ryujinx/ryujinx.mk
@@ -22,6 +22,10 @@ endef
 define RYUJINX_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/ryujinx
 	cp -pr $(@D)/target/publish/* $(TARGET_DIR)/usr/ryujinx
+
+	# alias, for compat, remove
+	rm $(TARGET_DIR)/usr/ryujinx/Ryujinx.Ava
+
 	# evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/ryujinx/switch.ryujinx.keys \
