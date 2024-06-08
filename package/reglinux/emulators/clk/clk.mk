31eecb042196e107090606387fdf5dcc63d0cb08
Romain TISSERAND <romain.tisserand@gmail.com>
2024-06-08 02:04:39 +0200

    Misc fixes: - Bump REG-Rescue to 0.4 - Remove vim - Fix clksignal missing install - Fix ryujinx "Ava" binary (same hash as main one) - Disable VCMI for now

--- package/reglinux/emulators/clk/clk.mk
+++ package/reglinux/emulators/clk/clk.mk
@@ -12,4 +12,7 @@ CLK_DEPENDENCIES = sdl2 libgl zlib
 
 CLK_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 
+define CLK_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/clksignal $(TARGET_DIR)/usr/bin/clksignal
+endef
 $(eval $(cmake-package))
