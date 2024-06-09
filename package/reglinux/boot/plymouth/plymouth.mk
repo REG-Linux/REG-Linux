f503f2d0d07380e7e0d938501d2aaed3025bd950
Juliano Dorigão <jdorigao@gmail.com>
2024-06-09 10:51:15 -0400

    plymouth: disabling dependencies

--- package/reglinux/boot/plymouth/plymouth.mk
+++ package/reglinux/boot/plymouth/plymouth.mk
@@ -7,8 +7,9 @@
 PLYMOUTH_VERSION = 24.004.60
 PLYMOUTH_SITE = https://gitlab.freedesktop.org/plymouth/plymouth.git
 PLYMOUTH_SITE_METHOD = git
+PLYMOUTH_DEPENDENCIES += libevdev libxkbcommon
 
-PLYMOUTH_CONF_OPTS = -Dgtk=disabled -Dsystemd-integration=false -Ddocs=false
-PLYMOUTH_DEPENDENCIES += pango libevdev libxkbcommon xkeyboard-config
+PLYMOUTH_CONF_OPTS  = -Dgtk=disabled -Dsystemd-integration=false -Ddocs=false
+PLYMOUTH_CONF_OPTS += -Dpango=disabled -Dfreetype=disabled
 
 $(eval $(meson-package))
