80978e44fb7a1f17ce7d1d0e0538c88b857efb78
Juliano Dorigão <jdorigao@gmail.com>
2024-06-10 10:22:51 -0400

    xxd: bump to v1.1 (ckormanyos)

--- package/batocera/utils/xxd/xxd.mk
+++ package/batocera/utils/xxd/xxd.mk
@@ -3,16 +3,20 @@
 # xxd
 #
 ################################################################################
+# Version.: Commits on Feb 19, 2024
+XXD_VERSION = v1.1
+XXD_SITE =  $(call github,ckormanyos,xxd,$(XXD_VERSION))
+XXD_LICENSE = GPL-2.0
+XXD_LICENSE_FILE = LICENSE
 
-XXD_VERSION = 084dc9dec6b6a6d048934916aa9a539d49ba898d
-XXD_SITE =  $(call github,ConorOG,xxd,$(XXD_VERSION))
-
-define HOST_XXD_BUILD_CMDS
-	$(MAKE) -C $(@D)
+define XXD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/xxd	$(TARGET_DIR)/usr/bin/xxd
 endef
+
+$(eval $(cmake-package))
 
 define HOST_XXD_INSTALL_CMDS
-	$(INSTALL) -D $(@D)/xxd $(HOST_DIR)/usr/bin/xxd
+	$(INSTALL) -D $(@D)/xxd	$(HOST_DIR)/usr/bin/xxd
 endef
 
-$(eval $(host-generic-package))
+$(eval $(host-cmake-package))
