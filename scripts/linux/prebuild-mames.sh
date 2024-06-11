a72c891a405d749754a66bee4718fd997138c232
Romain TISSERAND <romain.tisserand@gmail.com>
2024-06-12 00:21:11 +0200

    Disable mainline MAME and lr-mame for bcm2836 and other cortex a7 targets, just too slow

--- scripts/linux/prebuild-mames.sh
+++ scripts/linux/prebuild-mames.sh
@@ -23,12 +23,11 @@ mkdir -p prebuilt
 sed -i s/#BR2_PACKAGE_MAME_BUILD_FROM_SOURCE=y/BR2_PACKAGE_MAME_BUILD_FROM_SOURCE=y/ configs/batocera-board.common
 
 # Loop over archs
-# bcm2836 can be used for h3 (cortex_a7)
 # rk3326 can be used for s9gen4 (cortex_a35)
 # h5 can be used for h6, h616, s905, s905gen2, rk3328 (cortex_a53)
 # s905gen3 can be used for rk3566/rk3568 (cortex_a55)
 # s922x can be used for a3gen2 (cortex_a73_a53)
-for arch in s812 bcm2836 odroidxu4 rk3288 h5 bcm2711 bcm2712 rk3326 rk3399 rk3588 s905gen3 s922x saphira visionfive2 bpif3 x86_64 x86_64_v3; do
+for arch in s812 odroidxu4 rk3288 h5 bcm2711 bcm2712 rk3326 rk3399 rk3588 s905gen3 s922x saphira visionfive2 bpif3 x86_64 x86_64_v3; do
 	# Clean
 	make ${arch}-clean
 	# Build
