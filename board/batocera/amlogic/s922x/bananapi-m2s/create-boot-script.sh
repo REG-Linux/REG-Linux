18753b9f3ab48533f07baea03d16417f2ec472b8
Romain TISSERAND <romain.tisserand@gmail.com>
2024-07-02 00:03:33 +0200

    Preliminary support for BananaPi boards: - BananaPi M2S (S922X/A311D) - BananaPi M4 Berry (H618) - BananaPi M5 (S905X3) - BananaPi M7 (RK3588)

---
+++ board/batocera/amlogic/s922x/bananapi-m2s/create-boot-script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# HOST_DIR = host dir
+# BOARD_DIR = board specific dir
+# BUILD_DIR = base dir/build
+# BINARIES_DIR = images dir
+# TARGET_DIR = target dir
+# REGLINUX_BINARIES_DIR = reglinux binaries sub directory
+
+HOST_DIR=$1
+BOARD_DIR=$2
+BUILD_DIR=$3
+BINARIES_DIR=$4
+TARGET_DIR=$5
+REGLINUX_BINARIES_DIR=$6
+
+mkdir -p "${REGLINUX_BINARIES_DIR}/build-uboot-bananapi_m2s"     || exit 1
+cp "${BOARD_DIR}/build-uboot.sh"          "${REGLINUX_BINARIES_DIR}/build-uboot-bananapi_m2s/" || exit 1
+cd "${REGLINUX_BINARIES_DIR}/build-uboot-bananapi_m2s/" && ./build-uboot.sh "${HOST_DIR}" "${BOARD_DIR}" "${BINARIES_DIR}" || exit 1
+
+mkdir -p "${REGLINUX_BINARIES_DIR}/boot/boot"     || exit 1
+mkdir -p "${REGLINUX_BINARIES_DIR}/boot/extlinux" || exit 1
+
+cp "${BINARIES_DIR}/Image"              "${REGLINUX_BINARIES_DIR}/boot/boot/linux"              || exit 1
+cp "${BINARIES_DIR}/rootfs.cpio.lz4"    "${REGLINUX_BINARIES_DIR}/boot/boot/initrd.lz4"         || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs"    "${REGLINUX_BINARIES_DIR}/boot/boot/reglinux.update"    || exit 1
+cp "${BINARIES_DIR}/modules"            "${REGLINUX_BINARIES_DIR}/boot/boot/modules.update"     || exit 1
+cp "${BINARIES_DIR}/firmware"           "${REGLINUX_BINARIES_DIR}/boot/boot/firmware.update"    || exit 1
+cp "${BINARIES_DIR}/rescue"             "${REGLINUX_BINARIES_DIR}/boot/boot/rescue.update"      || exit 1
+
+cp "${BINARIES_DIR}/meson-g12b-a311d-bananapi-m2s.dtb" "${REGLINUX_BINARIES_DIR}/boot/boot/"       || exit 1
+cp "${BOARD_DIR}/boot/extlinux.conf"                   "${REGLINUX_BINARIES_DIR}/boot/extlinux/"   || exit 1
+
+exit 0
