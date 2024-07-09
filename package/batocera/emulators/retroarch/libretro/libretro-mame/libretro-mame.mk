0d57fd23134d8677815463630cd0c586ee80f74a
Romain TISSERAND <romain.tisserand@gmail.com>
2024-07-09 15:33:18 +0200

    Bump combo GroovyMame/lr-mame to 0.267 + REG-MAME prebuilt to 0.267

--- package/batocera/emulators/retroarch/libretro/libretro-mame/libretro-mame.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-mame/libretro-mame.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-# Version : Apr 27, 2024
-# lrmame0266
-LIBRETRO_MAME_VERSION = lrmame0266
+# Version : Jul 7, 2024
+# lrmame0267
+LIBRETRO_MAME_VERSION = lrmame0267
 LIBRETRO_MAME_SITE = $(call github,libretro,mame,$(LIBRETRO_MAME_VERSION))
 LIBRETRO_MAME_LICENSE = MAME
 
