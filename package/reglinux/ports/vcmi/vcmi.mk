d6cd9343044e56f061cba286a2d4a6bc5ea41d29
Romain TISSERAND <romain.tisserand@gmail.com>
2024-07-12 13:19:51 +0200

    Bump VCMI to 1.5.4

--- package/reglinux/ports/vcmi/vcmi.mk
+++ package/reglinux/ports/vcmi/vcmi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VCMI_VERSION = 1.5.3
+VCMI_VERSION = 1.5.4
 VCMI_SITE = https://github.com/vcmi/vcmi.git
 VCMI_SITE_METHOD=git
 VCMI_GIT_SUBMODULES=YES
