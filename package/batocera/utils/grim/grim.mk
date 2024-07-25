ad6a7df3142f7395bd4636653a436c1151c3acab
Romain TISSERAND <romain.tisserand@gmail.com>
2024-07-26 00:12:43 +0200

    Bump libopenmpt and grim

--- package/batocera/utils/grim/grim.mk
+++ package/batocera/utils/grim/grim.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GRIM_VERSION = 1.4.0
+GRIM_VERSION = 1.4.1
 GRIM_SOURCE = grim-$(GRIM_VERSION).tar.gz
 GRIM_SITE = https://git.sr.ht/~emersion/grim/refs/download/v$(GRIM_VERSION)
 GRIM_LICENSE = MIT
