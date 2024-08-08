5b99f3ec4d477700a1848c0e7f4d9705cd6b6c3b
Juliano Dorigão <jdorigao@gmail.com>
2024-08-08 10:26:26 -0400

    es-theme-canvas: added splash logo

--- package/reglinux/themes/es-theme-canvas/es-theme-canvas.mk
+++ package/reglinux/themes/es-theme-canvas/es-theme-canvas.mk
@@ -3,8 +3,8 @@
 # EmulationStation theme "Canvas"
 #
 ################################################################################
-# Version.: Commits on Aug 04, 2024
-ES_THEME_CANVAS_VERSION = c948b85531ad90df8f9fc869910ed038f9a3aff0
+# Version.: Commits on Aug 08, 2024
+ES_THEME_CANVAS_VERSION = 8d0d8f6d72da2472dba0f0db2b8a961bd001508a
 ES_THEME_CANVAS_SITE = $(call github,REG-Linux,es-theme-canvas,$(ES_THEME_CANVAS_VERSION))
 
 define ES_THEME_CANVAS_INSTALL_TARGET_CMDS
