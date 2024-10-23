33cac893b6e51002b09c009923f57ed99ad2e22e
Juliano Dorigão <jdorigao@gmail.com>
2024-10-23 17:51:48 -0400

    bump: libretro-flycast to v2.4

--- package/batocera/emulators/retroarch/libretro/libretro-flycast/libretro-flycast.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-flycast/libretro-flycast.mk
@@ -3,7 +3,7 @@
 # libretro-flycast
 #
 ################################################################################
-LIBRETRO_FLYCAST_VERSION = v2.3.2
+LIBRETRO_FLYCAST_VERSION = v2.4
 LIBRETRO_FLYCAST_SITE = https://github.com/flyinghead/flycast.git
 LIBRETRO_FLYCAST_SITE_METHOD=git
 LIBRETRO_FLYCAST_GIT_SUBMODULES=YES
