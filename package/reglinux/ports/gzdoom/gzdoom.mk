c288c370c7ffa84dede99dd6ec5c42af8e7cf67a
Romain TISSERAND <romain.tisserand@gmail.com>
2024-11-07 16:44:35 +0100

    Bump Cemu & GZDoom

--- package/reglinux/ports/gzdoom/gzdoom.mk
+++ package/reglinux/ports/gzdoom/gzdoom.mk
@@ -3,8 +3,8 @@
 # gzdoom
 #
 ################################################################################
-# Version: Commits on Oct 20, 2024
-GZDOOM_VERSION = g4.13.1
+# Version: Commits on Oct 31, 2024
+GZDOOM_VERSION = g4.13.2
 GZDOOM_SITE = https://github.com/ZDoom/gzdoom.git
 GZDOOM_SITE_METHOD=git
 GZDOOM_GIT_SUBMODULES=YES
