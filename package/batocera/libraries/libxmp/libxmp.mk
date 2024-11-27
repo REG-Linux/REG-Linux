413fed0c51c6cc1346d358c5bbbed8ebef5d04b7
Romain TISSERAND <romain.tisserand@gmail.com>
2024-11-27 19:28:55 +0100

    Bump buildroot + audio stack fixes

--- package/batocera/libraries/libxmp/libxmp.mk
+++ package/batocera/libraries/libxmp/libxmp.mk
@@ -5,7 +5,6 @@
 ################################################################################
 
 LIBXMP_VERSION = libxmp-4.6.0
-LIBXMP_SOURCE = enet-${LIBXMP_VERSION}.tar.gz
 LIBXMP_SITE =  $(call github,libxmp,libxmp,$(LIBXMP_VERSION))
 LIBXMP_INSTALL_STAGING = YES
 LIBXMP_AUTORECONF = YES
