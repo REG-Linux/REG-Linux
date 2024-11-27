de4d47a5758145f8ffad5440bbd9531bcefc685b
Romain TISSERAND <romain.tisserand@gmail.com>
2024-11-27 13:05:45 +0100

    Bump fheroes2 to 1.1.4

--- package/reglinux/ports/fheroes2/fheroes2.mk
+++ package/reglinux/ports/fheroes2/fheroes2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FHEROES2_VERSION = 1.1.3
+FHEROES2_VERSION = 1.1.4
 FHEROES2_SITE = $(call github,ihhub,fheroes2,$(FHEROES2_VERSION))
 FHEROES2_DEPENDENCIES = sdl2 sdl2_image
 
