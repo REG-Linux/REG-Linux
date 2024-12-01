450878db7b7b2cbe7a8d8db005cba15218929852
Romain TISSERAND <romain.tisserand@gmail.com>
2024-12-01 12:59:41 +0100

    system-vulkan script switch and improvements

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/citron/citronGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/citron/citronGenerator.py
@@ -156,15 +156,15 @@ class CitronGenerator(Generator):
             # Add vulkan logic
             if system.config["citron_backend"] == "1":
                 try:
-                    have_vulkan = subprocess.check_output(["/usr/bin/batocera-vulkan", "hasVulkan"], text=True).strip()
+                    have_vulkan = subprocess.check_output(["/usr/bin/system-vulkan", "hasVulkan"], text=True).strip()
                     if have_vulkan == "true":
                         eslog.debug("Vulkan driver is available on the system.")
                         try:
-                            have_discrete = subprocess.check_output(["/usr/bin/batocera-vulkan", "hasDiscrete"], text=True).strip()
+                            have_discrete = subprocess.check_output(["/usr/bin/system-vulkan", "hasDiscrete"], text=True).strip()
                             if have_discrete == "true":
                                 eslog.debug("A discrete GPU is available on the system. We will use that for performance")
                                 try:
-                                    discrete_index = subprocess.check_output(["/usr/bin/batocera-vulkan", "discreteIndex"], text=True).strip()
+                                    discrete_index = subprocess.check_output(["/usr/bin/system-vulkan", "discreteIndex"], text=True).strip()
                                     if discrete_index != "":
                                         eslog.debug("Using Discrete GPU Index: {} for citron".format(discrete_index))
                                         citronConfig.set("Renderer", "vulkan_device", discrete_index)
@@ -182,7 +182,7 @@ class CitronGenerator(Generator):
                         except subprocess.CalledProcessError:
                             eslog.debug("Error checking for discrete GPU.")
                 except subprocess.CalledProcessError:
-                    eslog.debug("Error executing batocera-vulkan script.")
+                    eslog.debug("Error executing system-vulkan script.")
         else:
             citronConfig.set("Renderer", "backend", "0")
         citronConfig.set("Renderer", "backend\\default", "false")
