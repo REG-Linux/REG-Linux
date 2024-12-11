16b4230c1b9ae3ec5959149fecdbd7cd78ba7fd7
Romain TISSERAND <romain.tisserand@gmail.com>
2024-12-11 21:02:55 +0100

    Bump eduke32 move to reg namespace

--- package/reglinux/ports/eduke32/eduke32.mk
+++ package/reglinux/ports/eduke32/eduke32.mk
@@ -3,19 +3,22 @@
 # eduke32
 #
 ################################################################################
-# Version: Commits on Jul 26, 2024
-EDUKE32_VERSION = 19c21b9ab10b0c17147c9ad951cc15279ed33f77
+# Version: Commits on Dec 8, 2024
+EDUKE32_VERSION = 9436bbf3e55763296e0f05d085cd3a21ba6a9d5c
 EDUKE32_SITE = https://voidpoint.io/terminx/eduke32/-/archive/$(EDUKE32_VERSION)
-EDUKE32_DEPENDENCIES = sdl2 flac libvpx
+EDUKE32_DEPENDENCIES = sdl2 flac
 EDUKE32_LICENSE = GPL-2.0
 
 # Some build options are documented here:
 # https://wiki.eduke32.com/wiki/Building_EDuke32_on_Linux
 EDUKE32_BUILD_ARGS = STARTUP_WINDOW=0
 EDUKE32_BUILD_ARGS += HAVE_GTK2=0
-ifeq ($(BR2_arm)$(BR2_aarch64)$(BR2_riscv),y)
+EDUKE32_BUILD_ARGS += OPTOPT="-ffast-math"
+ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
+    EDUKE32_BUILD_ARGS += USE_OPENGL=1 USE_LIBVPX=1
+    EDUKE32_BUILD_DEPENDENCIES += libvpx
+else
     EDUKE32_BUILD_ARGS += USE_OPENGL=0
-    EDUKE32_BUILD_ARGS += OPTOPT="-ffast-math"
 endif
 
 define EDUKE32_BUILD_CMDS
@@ -28,9 +31,9 @@ define EDUKE32_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/eduke32 $(TARGET_DIR)/usr/bin/eduke32
     $(INSTALL) -D -m 0755 $(@D)/fury $(TARGET_DIR)/usr/bin/fury
     mkdir -p $(TARGET_DIR)/usr/share/evmapy
-    cp $(BR2_EXTERNAL_REGLINUX_PATH)/package/batocera/ports/eduke32/eduke32.keys \
+    cp $(BR2_EXTERNAL_REGLINUX_PATH)/package/reglinux/ports/eduke32/eduke32.keys \
         $(TARGET_DIR)/usr/share/evmapy
-    cp $(BR2_EXTERNAL_REGLINUX_PATH)/package/batocera/ports/eduke32/eduke32.keys \
+    cp $(BR2_EXTERNAL_REGLINUX_PATH)/package/reglinux/ports/eduke32/eduke32.keys \
         $(TARGET_DIR)/usr/share/evmapy/fury.keys
 endef
 
