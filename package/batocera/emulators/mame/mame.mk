a0fec9ba5d2894b395ed659a30730190be3f2578
Romain TISSERAND <romain.tisserand@gmail.com>
2024-12-13 22:27:48 +0100

    Bump mame/lrmame/reglinux-mame combo to 0.272

--- package/batocera/emulators/mame/mame.mk
+++ package/batocera/emulators/mame/mame.mk
@@ -3,8 +3,8 @@
 # MAME (GroovyMAME)
 #
 ################################################################################
-# Version: GroovyMAME 0.271 - Switchres 2.21d
-MAME_VERSION = gm0271sr221d
+# Version: GroovyMAME 0.272 - Switchres 2.21d
+MAME_VERSION = gm0272sr221d
 MAME_SITE = $(call github,antonioginer,GroovyMAME,$(MAME_VERSION))
 MAME_DEPENDENCIES = sdl2 sdl2_ttf zlib libpng fontconfig sqlite jpeg flac rapidjson expat glm alsa-lib
 MAME_LICENSE = MAME
