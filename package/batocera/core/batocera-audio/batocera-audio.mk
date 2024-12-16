bfb70657f2132164e762a681ef19c0a2be361eca
Juliano Dorigão <jdorigao@gmail.com>
2024-12-16 08:46:55 -0400

    WIP on pipewire (Thanks Batocera)

--- package/batocera/core/batocera-audio/batocera-audio.mk
+++ package/batocera/core/batocera-audio/batocera-audio.mk
@@ -55,9 +55,9 @@ define BATOCERA_AUDIO_INSTALL_TARGET_CMDS
 		/usr/share/alsa/alsa.conf.d/{50-pipewire,99-pipewire-default}.conf
 
 	# pipewire-media-session config: disable dbus device reservation
-    mkdir -p $(TARGET_DIR)/usr/share/wireplumber/main.lua.d
-	cp $(BR2_EXTERNAL_REGLINUX_PATH)/package/batocera/core/batocera-audio/50-alsa-config.lua \
-		$(TARGET_DIR)/usr/share/wireplumber/main.lua.d/50-alsa-config.lua
+	mkdir -p $(TARGET_DIR)/usr/share/wireplumber/wireplumber.conf.d
+	cp $(BR2_EXTERNAL_REGLINUX_PATH)/package/batocera/core/batocera-audio/80-disable-alsa-reserve.conf \
+		$(TARGET_DIR)/usr/share/wireplumber/wireplumber.conf.d/80-disable-alsa-reserve.conf
 
 	cp $(BR2_EXTERNAL_REGLINUX_PATH)/package/batocera/core/batocera-audio/pipewire.conf \
 		$(TARGET_DIR)/usr/share/pipewire/pipewire.conf
