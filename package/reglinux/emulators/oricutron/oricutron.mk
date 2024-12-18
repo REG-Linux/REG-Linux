343cb320ff93b3e2df9ba0d6af868ab469ceb5da
Romain TISSERAND <romain.tisserand@gmail.com>
2024-12-18 23:19:13 +0100

    Oricutron 1.2.6 bump

--- package/reglinux/emulators/oricutron/oricutron.mk
+++ package/reglinux/emulators/oricutron/oricutron.mk
@@ -3,8 +3,8 @@
 # Oricutron - Oric computers emulator
 #
 ################################################################################
-# Version.: Release on Nov 19, 2023
-ORICUTRON_VERSION = V1_2_5
+# Version.: Release on Nov 7, 2024
+ORICUTRON_VERSION = V1_2_6
 ORICUTRON_SITE = https://github.com/pete-gordon/oricutron
 ORICUTRON_SITE_METHOD=git
 ORICUTRON_LICENSE = GPLv3
