f9ec33f791ea8684f9e123cfd414eb09cd97caac
Romain TISSERAND <romain.tisserand@gmail.com>
2024-12-20 11:37:07 +0100

    Add support for lgogdownloader

--- package/reglinux/utils/re2c/re2c.mk
+++ package/reglinux/utils/re2c/re2c.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RE2C_VERSION = 2.2
+RE2C_VERSION = 4.0.2
 RE2C_SITE =  $(call github,skvadrik,re2c,$(RE2C_VERSION))
 RE2C_AUTORECONF = YES
 
