6114080e5f6cac56e0986ec1ee0c5bd0f2c9b30e
Romain TISSERAND <romain.tisserand@gmail.com>
2025-01-16 16:37:41 +0100

    Bump lr-kronos to 2.7.0 official release + re-add lr-puae2021 for weak SoCs

---
+++ package/reglinux/emulators/libretro/libretro-puae2021/libretro-puae2021.mk
@@ -0,0 +1,35 @@
+################################################################################
+#
+# libretro-puae2021
+#
+################################################################################
+# Version: Commits on Jan 11, 2025
+LIBRETRO_PUAE2021_VERSION = 7d10191801c8d99223f8a9dbb9e421b78ae63949
+LIBRETRO_PUAE2021_SITE = $(call github,libretro,libretro-uae,$(LIBRETRO_PUAE2021_VERSION))
+LIBRETRO_PUAE2021_LICENSE = GPLv2
+
+LIBRETRO_PUAE2021_PLATFORM=$(LIBRETRO_PLATFORM)
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2835),y)
+LIBRETRO_PUAE2021_PLATFORM = rpi1
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2836),y)
+LIBRETRO_PUAE2021_PLATFORM = rpi2
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2837),y)
+LIBRETRO_PUAE2021_PLATFORM = rpi3_64
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2711),y)
+LIBRETRO_PUAE2021_PLATFORM = rpi4
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2712),y)
+LIBRETRO_PUAE2021_PLATFORM = rpi5
+endif
+
+define LIBRETRO_PUAE2021_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PUAE2021_PLATFORM)" \
+        GIT_VERSION="-$(shell echo $(LIBRETRO_PUAE2021_VERSION) | cut -c 1-7)"
+endef
+
+define LIBRETRO_PUAE2021_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/puae2021_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/puae2021_libretro.so
+endef
+
+$(eval $(generic-package))
