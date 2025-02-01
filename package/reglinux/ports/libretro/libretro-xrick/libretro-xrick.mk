b616420dbdea3659ce476d07203b60ae6f91f94e
Romain TISSERAND <romain.tisserand@gmail.com>
2025-02-01 12:34:54 +0100

    Move, bump some lr-ports

--- package/reglinux/ports/libretro/libretro-xrick/libretro-xrick.mk
+++ package/reglinux/ports/libretro/libretro-xrick/libretro-xrick.mk
@@ -3,8 +3,8 @@
 # libretro-xrick
 #
 ################################################################################
-# Version: Commits on Nov 1, 2023
-LIBRETRO_XRICK_VERSION = 58e544ee8de08ca8421e97c064d52ef6cdd73bd2
+# Version: Commits on Jan 5, 2025
+LIBRETRO_XRICK_VERSION = 476a9a637a6d2afa3f9f6b202bb98b522a4e95d1
 LIBRETRO_XRICK_SITE = $(call github,libretro,xrick-libretro,$(LIBRETRO_XRICK_VERSION))
 LIBRETRO_XRICK_LICENSE = GPL-3.0
 
