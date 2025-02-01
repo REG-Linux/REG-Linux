67b6a004b63c1c7fdb65a98f87b3cece9214ee15
Romain TISSERAND <romain.tisserand@gmail.com>
2025-02-01 11:23:37 +0100

    Restore EKA2L1 (for x86_64 only so far, will bring back SBC support later)

--- package/reglinux/emulators/eka2l1/eka2l1.mk
+++ package/reglinux/emulators/eka2l1/eka2l1.mk
@@ -6,14 +6,16 @@
 # Dirty version and repo while:
 # -fixing gcc13
 # - upstreaming needed changes (wayland, ffmpeg, gles)
-EKA2L1_VERSION = 8bf21e354c7aa3ea8f287a5f47825d1370b1c14f
-EKA2L1_SITE = https://github.com/rtissera/EKA2L1.git
+EKA2L1_VERSION = e67f84dc605ea30afc1ab6f4f43c0f855eec79a5
+EKA2L1_SITE = https://github.com/EKA2L1/EKA2L1.git
 EKA2L1_SITE_METHOD=git
 EKA2L1_GIT_SUBMODULES=YES
 EKA2L1_LICENSE = GPLv2
 EKA2L1_DEPENDENCIES = reglinux-qt6
 
 EKA2L1_SUPPORTS_IN_SOURCE_BUILD = NO
+
+EKA2L1_CONF_ENV = CFLAGS="-Wno-error=incompatible-pointer-types -Wno-error=int-conversion" CXXFLAGS="-Wno-error=incompatible-pointer-types -Wno-error=int-conversion"
 
 EKA2L1_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 EKA2L1_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
