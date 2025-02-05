e57e8f167512a64ba5a8a0aed037a5b49f59c20e
cjom <55603581+cjom@users.noreply.github.com>
2025-02-05 22:56:02 +0000

    Avoid using FUSE version of exFAT and NTFS drivers
    - Only use ntfs-3g with kernel  5.4 or 5..10 and exFAT (fuse) with kernel 5.4
       This fixes SSH not working when using exFAT SHARE partition.
    - Make some kernel modules similar on all devices.
    - Make file system options uniform between ES and REG-rescue.
    - system-mount tries to fix dirty NTFS partitions (they fail when mounted).
    - Bump  REG-rescue (fixes severe bug when formatting ntfs).

--- package/reglinux/reglinux-ntfs/reglinux-ntfs.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-#
-# REG-Linux-NTFS - NTFS implementation using either FUSE or in-kernel support
-#
-################################################################################
-
-REGLINUX_NTFS_VERSION = 0.1
-
-$(eval $(virtual-package))
