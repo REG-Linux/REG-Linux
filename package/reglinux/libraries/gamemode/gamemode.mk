9706fb317f02982993792a546b2db73b9f8fce86
Romain TISSERAND <romain.tisserand@gmail.com>
2025-02-09 15:01:46 +0100

    Parallel build WIP

--- package/reglinux/libraries/gamemode/gamemode.mk
+++ package/reglinux/libraries/gamemode/gamemode.mk
@@ -6,6 +6,7 @@
 GAMEMODE_VERSION = 1.8.2
 GAMEMODE_SITE = $(call github,FeralInteractive,gamemode,$(GAMEMODE_VERSION))
 GAMEMODE_INSTALL_STAGING = YES
+GAMEMODE_DEPENDENCIES = dbus
 
 GAMEMODE_CONF_OPTS = -Dwith-sd-bus-provider=no-daemon -Dwith-examples=false
 
