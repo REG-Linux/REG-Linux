84cd41a94af21cfb948903329632fc28b863e40c
Romain TISSERAND <romain.tisserand@gmail.com>
2025-02-10 18:53:35 +0100

    Bump DevilutionX to 1.5.4

--- package/reglinux/ports/devilutionx/devilutionx.mk
+++ package/reglinux/ports/devilutionx/devilutionx.mk
@@ -4,17 +4,19 @@
 #
 ################################################################################
 
-# Version: Release 1.5.3 - Sep 1, 2024
-DEVILUTIONX_VERSION = 1.5.3
+# Version: Release 1.5.4 - Feb 9, 2025
+DEVILUTIONX_VERSION = 1.5.4
 DEVILUTIONX_SITE = https://github.com/diasurgical/devilutionX/releases/download/$(DEVILUTIONX_VERSION)
 DEVILUTIONX_SOURCE = devilutionx-src.tar.xz
 DEVILUTIONX_DEPENDENCIES = sdl2 sdl2_image fmt libsodium libpng bzip2
 DEVILUTIONX_SUPPORTS_IN_SOURCE_BUILD = NO
 
+DEVILUTIONX_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+DEVILUTIONX_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+DEVILUTIONX_CONF_OPTS += -DBUILD_STATIC_LIBS=ON
 # Prefill the player name when creating a new character, in case the device does
 # not have a keyboard.
-DEVILUTIONX_CONF_OPTS += -DBUILD_TESTING=OFF -DPREFILL_PLAYER_NAME=ON \
-                         -DCMAKE_BUILD_TYPE=Release
+DEVILUTIONX_CONF_OPTS += -DBUILD_TESTING=OFF -DPREFILL_PLAYER_NAME=ON
 
 # Ensure that DevilutionX's vendored dependencies are not accidentally fetched from network.
 # They should all be present in the source package.
