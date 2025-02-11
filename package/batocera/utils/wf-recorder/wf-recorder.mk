e8c35d823d6277baacfb7d39619e209dba856f1f
Romain TISSERAND <romain.tisserand@gmail.com>
2025-02-11 16:14:49 +0100

    Fix more parallel build issues

--- package/batocera/utils/wf-recorder/wf-recorder.mk
+++ package/batocera/utils/wf-recorder/wf-recorder.mk
@@ -8,7 +8,29 @@ WF_RECORDER_VERSION = v0.5.0
 WF_RECORDER_SITE = $(call github,ammen99,wf-recorder,$(WF_RECORDER_VERSION))
 WF_RECORDER_LICENSE = MIT
 WF_RECORDER_LICENSE_FILES = LICENSE
-WF_RECORDER_DEPENDENCIES = ffmpeg pipewire
+WF_RECORDER_DEPENDENCIES = ffmpeg
 
+ifeq ($(BR2_PACKAGE_X264),y)
+WF_RECORDER_DEPENDENCIES += x264
+endif
+
+ifeq ($(BR2_PACKAGE_X265),y)
+WF_RECORDER_DEPENDENCIES += x265
+endif
+
+ifeq ($(BR2_PACKAGE_PULSEAUDIO),y)
+WF_RECORDER_CONF_OPTS = -Dpulse=enabled
+WF_RECORDER_DEPENDENCIES += pulseaudio
+else
+WF_RECORDER_CONF_OPTS = -Dpulse=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_PIPEWIRE),y)
+WF_RECORDER_CONF_OPTS = -Dpipewire=enabled
+WF_RECORDER_CONF_OPTS = -Ddefault_audio_backend=pipewire
+WF_RECORDER_DEPENDENCIES += pipewire
+else
+WF_RECORDER_CONF_OPTS = -Dpipewire=disabled
+endif
 
 $(eval $(meson-package))
