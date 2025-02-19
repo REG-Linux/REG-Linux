dc530fda5199a62238db898f91f70446de866919
Romain TISSERAND <romain.tisserand@gmail.com>
2025-02-19 16:47:10 +0100

    Bump emulators and ports

--- package/reglinux/emulators/lightspark/lightspark.mk
+++ package/reglinux/emulators/lightspark/lightspark.mk
@@ -4,21 +4,35 @@
 #
 ################################################################################
 # Version.: Commits on Feb 16, 2025
+# GLES support broken in 0.9.0 so far, so we use 0.8.7 for GLES-only targets
+ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
 LIGHTSPARK_VERSION = 0.9.0
+else
+LIGHTSPARK_VERSION = 0.8.7
+endif
 LIGHTSPARK_SITE = $(call github,lightspark,lightspark,$(LIGHTSPARK_VERSION))
 LIGHTSPARK_LICENSE = LGPLv3
 LIGHTSPARK_DEPENDENCIES = sdl2 freetype pcre jpeg libpng cairo pango ffmpeg libcurl rtmpdump
 
+# Do not build browser plugin(s)
 LIGHTSPARK_CONF_OPTS += -DCOMPILE_NPAPI_PLUGIN=FALSE -DCOMPILE_PPAPI_PLUGIN=FALSE
+
+# OpenGL support
 ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
 LIGHTSPARK_DEPENDENCIES += libgl libglew
 else
 LIGHTSPARK_CONF_OPTS += -DENABLE_GLES2=TRUE -DCMAKE_C_FLAGS=-DEGL_NO_X11 -DCMAKE_CXX_FLAGS=-DEGL_NO_X11
 endif
 
+# Pass proper architecture
 LIGHTSPARK_ARCH = $(BR2_ARCH)
 ifeq ($(LIGHTSPARK_ARCH), "arm")
 LIGHTSPARK_ARCH = armv7l
+endif
+
+# Fastpaths for x86_64
+ifeq ($(BR2_x86_64),y)
+LIGHTSPARK_CONF_OPTS += -DENABLE_SSE2=TRUE
 endif
 
 define LIGHTSPARK_INSTALL_TARGET_CMDS
