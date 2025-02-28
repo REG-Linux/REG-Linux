32a0be3a390271b3988b3360404d4aadad0f7229
Romain TISSERAND <romain.tisserand@gmail.com>
2025-02-28 11:11:45 +0100

    Fix xenia-canary build

--- package/reglinux/emulators/xenia-canary/xenia-canary.mk
+++ package/reglinux/emulators/xenia-canary/xenia-canary.mk
@@ -20,7 +20,7 @@ define XENIA_CANARY_CONFIGURE_CMDS
 	SYSROOT="$(STAGING_DIR)" \
 	SDL2CONFIG="$(HOST_DIR)/bin/sdl2-config" \
 	CMAKE_MAKE_PROGRAM="$(HOST_DIR)/bin/ninja" \
-	./xb premake #setup --target_os Linux
+	./xb premake
 endef
 
 define XENIA_CANARY_BUILD_CMDS
@@ -33,27 +33,25 @@ define XENIA_CANARY_BUILD_CMDS
 	CC="$(HOST_DIR)/bin/clang" \
 	CXX="$(HOST_DIR)/bin/clang++" \
 	./xb build --config release
-#PATH=$(HOST_DIR)/bin:$(PATH) 
-	#TODO pass GCC, only Clang seems supported so far --cc gcc
-
-
 endef
 
 define XENIA_CANARY_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr
-	rsync -av --exclude=".*" $(@D)/build/bin/Linux/Release/ $(TARGET_DIR)/usr/xenia-canary/
+	# Copy stripped binary
+	mkdir -p $(TARGET_DIR)/usr/bin
+	$(TARGET_STRIP) $(@D)/build/bin/Linux/Release/xenia_canary
+	cp $(@D)/build/bin/Linux/Release/xenia_canary $(TARGET_DIR)/usr/bin/
 endef
 
 define XENIA_CANARY_POST_PROCESS
 	# get the latest patches
-	mkdir -p $(TARGET_DIR)/usr/xenia-canary/patches
+	mkdir -p $(TARGET_DIR)/usr/share/xenia-canary/patches
 	mkdir -p $(@D)/temp
 	( cd $(@D)/temp && $(GIT) init && \
 	  $(GIT) remote add origin https://github.com/xenia-canary/game-patches.git && \
 	  $(GIT) config core.sparsecheckout true && \
 	  echo "patches/*.toml" >> .git/info/sparse-checkout && \
 	  $(GIT) pull --depth=1 origin main && \
-	  mv -f patches/*.toml $(TARGET_DIR)/usr/xenia-canary/patches \
+	  mv -f patches/*.toml $(TARGET_DIR)/usr/share/xenia-canary/patches \
 	)
 
 	# Clean up the temporary directory
