02eb5a2cf39a403ceaade07df903b58671d072ae
Romain TISSERAND <romain.tisserand@gmail.com>
2025-03-03 13:54:13 +0100

    Fix host-xa

--- package/reglinux/utils/xa/xa.mk
+++ package/reglinux/utils/xa/xa.mk
@@ -4,16 +4,15 @@
 #
 ################################################################################
 
-XA_VERSION = 2.4.1
-XA_SOURCE=xa-$(XA_VERSION).tar.gz
-XA_SITE = https://www.floodgap.com/retrotech/xa/dists
+XA_VERSION = xa-2.4.1
+XA_SITE = $(call github,fachat,xa65,$(XA_VERSION))
 
 define HOST_XA_BUILD_CMDS
-	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) -f Makefile
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)/xa -f Makefile
 endef
 
 define HOST_XA_INSTALL_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/xa $(HOST_DIR)/usr/bin/xa ;
+	$(INSTALL) -D -m 0755 $(@D)/xa/xa $(HOST_DIR)/usr/bin/xa ;
 endef
 
 $(eval $(host-generic-package))
