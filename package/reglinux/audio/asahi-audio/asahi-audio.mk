deb8cde1879b74ebede342ebccda7cf61f42616a
Romain TISSERAND <romain.tisserand@gmail.com>
2025-03-05 23:15:59 +0100

    WIP: SDL3, mednafen, lightspark, xenia-canary, asahi audio stack

--- package/reglinux/audio/asahi-audio/asahi-audio.mk
+++ package/reglinux/audio/asahi-audio/asahi-audio.mk
@@ -4,10 +4,14 @@
 #
 ################################################################################
 
-ASAHI_AUDIO_VERSION = v3.0
+ASAHI_AUDIO_VERSION = v2.6
 ASAHI_AUDIO_SITE = $(call github,AsahiLinux,asahi-audio,$(ASAHI_AUDIO_VERSION))
 ASAHI_AUDIO_LICENSE = BSD-3-Clause
 ASAHI_AUDIO_LICENSE_FILES = LICENSE
 ASAHI_AUDIO_DEPENDENCIES = pipewire wireplumber alsa-ucm-conf asahi-alsa-ucm-conf
 
+define ASAHI_AUDIO_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share
+	cd $(@D) && DESTDIR=$(TARGET_DIR) make install
+endef
 $(eval $(generic-package))
