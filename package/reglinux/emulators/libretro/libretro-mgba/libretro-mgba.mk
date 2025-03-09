00137fa199252111dc063b1ae013ab5b128eac24
Romain TISSERAND <romain.tisserand@gmail.com>
2025-03-09 18:26:51 +0100

    Bump lr-mgba

--- package/reglinux/emulators/libretro/libretro-mgba/libretro-mgba.mk
+++ package/reglinux/emulators/libretro/libretro-mgba/libretro-mgba.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Dec 8, 2024
-LIBRETRO_MGBA_VERSION = 0.10.4
+LIBRETRO_MGBA_VERSION = 0.10.5
 LIBRETRO_MGBA_SITE = $(call github,mgba-emu,mgba,$(LIBRETRO_MGBA_VERSION))
 LIBRETRO_MGBA_LICENSE = MPLv2.0
 
