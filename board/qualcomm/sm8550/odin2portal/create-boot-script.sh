e3c8f7e604a4dc2f5872f60da8abe8fef73c492e
Romain TISSERAND <romain.tisserand@gmail.com>
2025-03-18 02:26:09 +0100

    Boots to ES, wrong orientation, no sound, no wifi/bt

--- board/qualcomm/sm8550/odin2portal/create-boot-script.sh
+++ board/qualcomm/sm8550/odin2portal/create-boot-script.sh
@@ -16,7 +16,7 @@ REGLINUX_BINARIES_DIR=$6
 
 mkdir -p "${REGLINUX_BINARIES_DIR}/boot/boot"     || exit 1
 
-cp "${BINARIES_DIR}/Image"              "${REGLINUX_BINARIES_DIR}/boot/boot/Image"           || exit 1
+cp "${BINARIES_DIR}/Image"              "${REGLINUX_BINARIES_DIR}/boot/boot/linux"           || exit 1
 cp "${BINARIES_DIR}/rootfs.cpio.lz4"    "${REGLINUX_BINARIES_DIR}/boot/boot/initrd.lz4"      || exit 1
 cp "${BINARIES_DIR}/rootfs.squashfs"    "${REGLINUX_BINARIES_DIR}/boot/boot/reglinux.update" || exit 1
 cp "${BINARIES_DIR}/modules"            "${REGLINUX_BINARIES_DIR}/boot/boot/modules.update"  || exit 1
@@ -25,7 +25,5 @@ cp "${BINARIES_DIR}/rescue"             "${REGLINUX_BINARIES_DIR}/boot/boot/resc
 
 cp "${BINARIES_DIR}/qcs8550-ayn-odin2portal.dtb"        "${REGLINUX_BINARIES_DIR}/boot/boot/"           || exit 1
 cp -f "${BOARD_DIR}/LinuxLoader.cfg"        "${REGLINUX_BINARIES_DIR}/boot/LinuxLoader.cfg"             || exit 1
-#cp -f "${BOARD_DIR}/grub.cfg"                   "${BINARIES_DIR}/efi-part/EFI/BOOT/grub.cfg"    || exit 1
-#cp -r "${BINARIES_DIR}/efi-part/EFI/"           "${REGLINUX_BINARIES_DIR}/boot/"                || exit 1
 
 exit 0
