9a360799e443307e435cd1f6e084a28192aac35c
Juliano Dorigão <jdorigao@gmail.com>
2025-03-24 11:14:41 -0400

    Environment variables (wayland/drm)
    Set environment variables at startup according to the graphical environment.

--- package/system/reglinux-system/xdg.sh
+++ package/system/reglinux-system/xdg.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 export XDG_RUNTIME_DIR=/var/run
+
+environment="$(/usr/bin/system-settings-get-master system.es.environment)"
+
+if [ "$environment" = "wayland" ]; then
+	export XDG_SESSION_TYPE=wayland
+	export SDL_VIDEO_DRIVER=wayland
+	export QT_QPA_PLATFORM=wayland
+else
+	export XDG_SESSION_TYPE=drm
+	export QT_QPA_PLATFORM=xcb
+fi
