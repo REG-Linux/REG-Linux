e2ee1fa89f37eeaca46ab87fd4d4bbdf0fcdea68
Romain TISSERAND <romain.tisserand@gmail.com>
2025-03-25 11:51:15 +0100

    Bump alllinuxfirmwares, enable SDL3 tests (+install) to ease gamepad mapping

--- package/utils/wlr-randr/wlr-randr.mk
+++ package/utils/wlr-randr/wlr-randr.mk
@@ -4,6 +4,7 @@
 #
 ################################################################################
 
+# Release 0.5.0 on - Feb 12, 2025
 WLR_RANDR_VERSION = 0.5.0
 WLR_RANDR_SITE = https://gitlab.freedesktop.org/emersion/wlr-randr/-/releases/v$(WAYLAND_VERSION)/downloads
 WLR_RANDR_SOURCE = wlr-randr-$(WLR_RANDR_VERSION).tar.gz
