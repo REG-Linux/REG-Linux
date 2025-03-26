56104abe4d394db7361a586d591ed3be6cbf22ef
Romain TISSERAND <romain.tisserand@gmail.com>
2025-03-26 17:34:34 +0100

    Some build fixes and bumps

--- package/libraries/librashader/librashader.mk
+++ package/libraries/librashader/librashader.mk
@@ -10,6 +10,8 @@ LIBRASHADER_LICENSE = GPLv3
 
 LIBRASHADER_DEPENDENCIES = host-rustc host-rust-bin libglfw
 
+LIBRASHADER_CARGO_MODE = $(if $(BR2_ENABLE_DEBUG),debug,release)
+
 LIBRASHADER_ARCH = $(BR2_ARCH)
 ifeq ($(LIBRASHADER_ARCH),"riscv64")
 LIBRASHADER_ARCH = riscv64gc
@@ -20,7 +22,7 @@ LIBRASHADER_INSTALL_STAGING = YES
 LIBRASHADER_CARGO_BUILD_OPTS += --features stable
 
 define LIBRASHADER_INSTALL_STAGING_CMDS
-	cd $(@D)/target/$(LIBRASHADER_ARCH)-unknown-linux-gnu/release/ && \
+	cd $(@D)/target/$(LIBRASHADER_ARCH)-unknown-linux-gnu/$(LIBRASHADER_CARGO_MODE)/ && \
 	cp liblibrashader_capi.so $(STAGING_DIR)/usr/lib/librashader.so.2 && \
 	cd $(STAGING_DIR) && ln -sf librashader.so.2 librashader.so && \
 	cp $(@D)/pkg/librashader.pc $(STAGING_DIR)/usr/lib/pkgconfig/librashader.pc && \
@@ -29,7 +31,7 @@ define LIBRASHADER_INSTALL_STAGING_CMDS
 endef
 
 define LIBRASHADER_INSTALL_TARGET_CMDS
-	cd $(@D)/target/$(LIBRASHADER_ARCH)-unknown-linux-gnu/release/ && \
+	cd $(@D)/target/$(LIBRASHADER_ARCH)-unknown-linux-gnu/$(LIBRASHADER_CARGO_MODE)/ && \
 	cp liblibrashader_capi.so $(TARGET_DIR)/usr/lib/librashader.so.2 && \
 	cd $(TARGET_DIR) && ln -sf librashader.so.2 librashader.so
 endef
