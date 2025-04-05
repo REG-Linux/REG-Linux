ec498722605e1ce5bfd38ba51cdc3f326e8aae20
Romain TISSERAND <romain.tisserand@gmail.com>
2025-04-05 16:52:24 +0200

    Add lr-sameboy (high accuracy GB/GBC core) for all 64-bit architectures

---
+++ package/emulators/libretro/libretro-sameboy/libretro-sameboy.mk
@@ -0,0 +1,26 @@
+
+################################################################################
+#
+# libretro-sameboy
+#
+################################################################################
+# Version: Release 1.0.1 on Apr 5, 2024
+LIBRETRO_SAMEBOY_VERSION = v1.0.1
+LIBRETRO_SAMEBOY_SITE = $(call github,LIJI32,SameBoy,$(LIBRETRO_SAMEBOY_VERSION))
+LIBRETRO_SAMEBOY_LICENSE = Expat
+LIBRETRO_SAMEBOY_DEPENDENCIES = host-rgbds
+
+# Improve plaform selection
+LIBRETRO_SAMEBOY_PLATFORM = unix
+
+define LIBRETRO_SAMEBOY_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) \
+	    -f Makefile platform="$(LIBRETRO_SAMEBOY_PLATFORM)" bootroms libretro
+endef
+
+define LIBRETRO_SAMEBOY_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/build/bin/sameboy_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/sameboy_libretro.so
+endef
+
+$(eval $(generic-package))
