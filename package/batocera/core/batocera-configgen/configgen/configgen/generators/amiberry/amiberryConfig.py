abcf8a82ef70c204a0ee1d5083612211ed251c37
Juliano Dorigão <jdorigao@gmail.com>
2025-04-13 00:11:27 -0400

    configgen: amiberry

---
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/amiberry/amiberryConfig.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import batoceraFiles
+
+amiberryRoot = batoceraFiles.CONF + '/amiberry'
+amiberryRetroarchInputsDir = amiberryRoot + '/conf/retroarch/inputs'
+amiberryRetroarchCustom = amiberryRoot + '/conf/retroarch/retroarchcustom.cfg'
