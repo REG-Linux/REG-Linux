fdf8b9ea5eefd3206c3c691dfe9c4afca1c2e6c6
cjom <55603581+cjom@users.noreply.github.com>
2025-04-13 23:32:16 +0100

    Fix retroarch-joystick-autoconfig target paths

--- package/retroarch/retroarch-joypad-autoconfig/retroarch-joypad-autoconfig.mk
+++ package/retroarch/retroarch-joypad-autoconfig/retroarch-joypad-autoconfig.mk
@@ -8,11 +8,11 @@ RETROARCH_JOYPAD_AUTOCONFIG_VERSION = v1.20.0
 RETROARCH_JOYPAD_AUTOCONFIG_SITE = $(call github,libretro,retroarch-joypad-autoconfig,$(RETROARCH_JOYPAD_AUTOCONFIG_VERSION))
 RETROARCH_JOYPAD_AUTOCONFIG_LICENSE = MIT
 
-RETROARCH_JOYPAD_AUTOCONFIG_INSTALL_DIR = $(TARGET_DIR)/usr/share/libretro/autoconfig
+RETROARCH_JOYPAD_AUTOCONFIG_INSTALL_DIR = $(TARGET_DIR)/usr/share/reglinux/datainit/system/configs/retroarch/inputs
 
 define RETROARCH_JOYPAD_AUTOCONFIG_INSTALL_TARGET_CMDS
 	# Stock config files from libretro
-	mkdir -p $(TARGET_DIR)/usr/share/libretro/autoconfig
+	mkdir -p $(RETROARCH_JOYPAD_AUTOCONFIG_INSTALL_DIR)
 	cp -r $(@D)/hid		$(RETROARCH_JOYPAD_AUTOCONFIG_INSTALL_DIR)
 	cp -r $(@D)/linuxraw	$(RETROARCH_JOYPAD_AUTOCONFIG_INSTALL_DIR)
 	cp -r $(@D)/sdl2	$(RETROARCH_JOYPAD_AUTOCONFIG_INSTALL_DIR)
@@ -21,7 +21,7 @@ define RETROARCH_JOYPAD_AUTOCONFIG_INSTALL_TARGET_CMDS
 	cp -r $(@D)/xinput	$(RETROARCH_JOYPAD_AUTOCONFIG_INSTALL_DIR)
 
 	# Borrow additional ones from ROCKNIX
-	cp -r $(BR2_EXTERNAL_REGLINUX_PATH)/package/retroarch/retroarch-joypad-autoconfig/rocknix/* $(RETROARCH_JOYPAD_AUTOCONFIG_INSTALL_DIR)
+	cp -r $(BR2_EXTERNAL_REGLINUX_PATH)/package/retroarch/retroarch-joypad-autoconfig/rocknix/* $(RETROARCH_JOYPAD_AUTOCONFIG_INSTALL_DIR)/udev/
 endef
 
 $(eval $(generic-package))
