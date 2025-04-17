146e9dc545c39e7042f9b63831cb23c9d89718d1
Juliano Dorigão <jdorigao@gmail.com>
2025-04-16 22:26:42 -0400

    configgen: flycast (controllers)

--- package/system/reglinux-configgen/configgen/configgen/generators/flycast/flycastGenerator.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/flycast/flycastGenerator.py
@@ -9,7 +9,6 @@ import controllersConfig
 from shutil import copyfile
 from os.path import isdir
 from os.path import isfile
-from . import flycastControllers
 from . import flycastConfig
 
 class FlycastGenerator(Generator):
@@ -28,53 +27,38 @@ class FlycastGenerator(Generator):
 
         if not Config.has_section("input"):
             Config.add_section("input")
-        # For each pad detected
-        for index in playersControllers:
-            controller = playersControllers[index]
-            # Write the mapping files for Dreamcast
-            if (system.name == "dreamcast"):
-                flycastControllers.generateControllerConfig(controller, "dreamcast")
-            else:
-                # Write the Arcade variant (Atomiswave & Naomi/2)
-                flycastControllers.generateControllerConfig(controller, "arcade")
-
-            # Set the controller type per Port
-            Config.set("input", 'device' + str(controller.player), "0") # Sega Controller
-            Config.set("input", 'device' + str(controller.player) + '.1', "1") # Sega VMU
-            # Set controller pack, gui option
-            ctrlpackconfig = "flycast_ctrl{}_pack".format(controller.player)
-            if system.isOptSet(ctrlpackconfig):
-                Config.set("input", 'device' + str(controller.player) + '.2', str(system.config[ctrlpackconfig]))
-            else:
-                Config.set("input", 'device' + str(controller.player) + '.2', "1") # Sega VMU
-            # Ensure controller(s) are on seperate Ports
-            port = int(controller.player)-1
-            Config.set("input", 'maple_sdl_joystick_' + str(port), str(port))
 
         if not Config.has_section("config"):
             Config.add_section("config")
+
         if not Config.has_section("window"):
             Config.add_section("window")
+
         # ensure we are always fullscreen
         Config.set("window", "fullscreen", "yes")
+
         # set video resolution
         Config.set("window", "width", str(gameResolution["width"]))
         Config.set("window", "height", str(gameResolution["height"]))
+
         # set render resolution - default 480 (Native)
         if system.isOptSet("flycast_render_resolution"):
             Config.set("config", "rend.Resolution", str(system.config["flycast_render_resolution"]))
         else:
             Config.set("config", "rend.Resolution", "480")
+
         # wide screen mode - default off
         if system.isOptSet("flycast_ratio"):
             Config.set("config", "rend.WideScreen", str(system.config["flycast_ratio"]))
         else:
             Config.set("config", "rend.WideScreen", "no")
+
         # rotate option - default off
         if system.isOptSet("flycast_rotate"):
             Config.set("config", "rend.Rotate90", str(system.config["flycast_rotate"]))
         else:
             Config.set("config", "rend.Rotate90", "no")
+
         # renderer - default: OpenGL
         if system.isOptSet("flycast_renderer") and system.config["flycast_renderer"] == "0":
             if system.isOptSet("flycast_sorting") and system.config["flycast_sorting"] == "3":
@@ -100,6 +84,7 @@ class FlycastGenerator(Generator):
             Config.set("config", "rend.AnisotropicFiltering", str(system.config["flycast_anisotropic"]))
         else:
             Config.set("config", "rend.AnisotropicFiltering", "1")
+
         # transparent sorting
         # per strip
         if system.isOptSet("flycast_sorting") and system.config["flycast_sorting"] == "2":
@@ -113,11 +98,13 @@ class FlycastGenerator(Generator):
             Config.set("config", "Dreamcast.Language", str(system.config["flycast_language"]))
         else:
             Config.set("config", "Dreamcast.Language", "1")
+
         # region
         if system.isOptSet("flycast_region"):
             Config.set("config", "Dreamcast.Region", str(system.config["flycast_language"]))
         else:
             Config.set("config", "Dreamcast.Region", "1")
+
         # save / load states
         if system.isOptSet("flycast_loadstate"):
             Config.set("config", "Dreamcast.AutoLoadState", str(system.config["flycast_loadstate"]))
@@ -127,16 +114,19 @@ class FlycastGenerator(Generator):
             Config.set("config", "Dreamcast.AutoSaveState", str(system.config["flycast_savestate"]))
         else:
             Config.set("config", "Dreamcast.AutoSaveState", "no")
+
         # windows CE
         if system.isOptSet("flycast_winCE"):
             Config.set("config", "Dreamcast.ForceWindowsCE", str(system.config["flycast_winCE"]))
         else:
             Config.set("config", "Dreamcast.ForceWindowsCE", "no")
+
         # DSP
         if system.isOptSet("flycast_DSP"):
              Config.set("config", "aica.DSPEnabled", str(system.config["flycast_DSP"]))
         else:
             Config.set("config", "aica.DSPEnabled", "no")
+
         # Guns (WIP)
         # Guns crosshairs
         if system.isOptSet("flycast_lightgun1_crosshair"):
