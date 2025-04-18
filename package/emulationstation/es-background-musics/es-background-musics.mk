b05e6c2ce5dfa96142c35dbaa27c8e3488f7bb9c
Juliano Dorigão <jdorigao@gmail.com>
2025-04-18 09:43:16 -0400

    moved: es-background-musics

--- package/emulationstation/es-background-musics/es-background-musics.mk
+++ package/emulationstation/es-background-musics/es-background-musics.mk
@@ -8,10 +8,11 @@ ES_BACKGROUND_MUSICS_VERSION = 3.0
 ES_BACKGROUND_MUSICS_LICENSE = Public Domain
 ES_BACKGROUND_MUSICS_SOURCE=
 
+ES_BACKGROUND_MUSICS_PATH = $(BR2_EXTERNAL_REGLINUX_PATH)/package/emulationstation/es-background-musics
+
 define ES_BACKGROUND_MUSICS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/reglinux/music
-
-	cp -R $(BR2_EXTERNAL_REGLINUX_PATH)/package/batocera/emulationstation/es-background-musics/music/* $(TARGET_DIR)/usr/share/reglinux/music/
+	cp -R $(ES_BACKGROUND_MUSICS_PATH)/music/* $(TARGET_DIR)/usr/share/reglinux/music/
 endef
 
 $(eval $(generic-package))
