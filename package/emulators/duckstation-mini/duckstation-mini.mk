2f519e8ca871e28db7a5b5c4a5148fbba7d47b6a
Romain TISSERAND <romain.tisserand@gmail.com>
2025-04-24 00:01:04 +0200

    Rework PSX cores and DuckStation: - Remove duckstation-legacy - Rename duckstation to duckstation-gpl - Introduce duckstation (appimage x86_64/armhf/arm64) - Introduce duckstation-mini (appimage armhf/arm64) reglinux-configgen will need some adjustments to handle appimage files

---
+++ package/emulators/duckstation-mini/duckstation-mini.mk
@@ -0,0 +1,32 @@
+################################################################################
+#
+# DuckStation Mini (AppImage) - Rolling release
+#
+################################################################################
+DUCKSTATION_MINI_VERSION = v0.1-8891
+ifeq ($(BR2_arm),y)
+DUCKSTATION_MINI_SOURCE = DuckStation-Mini-armhf.AppImage
+else ifeq ($(BR2_aarch64),y)
+DUCKSTATION_MINI_SOURCE = DuckStation-Mini-arm64.AppImage
+endif
+DUCKSTATION_MINI_SITE = https://github.com/stenzek/duckstation/releases/download/$(DUCKSTATION_MINI_VERSION)
+DUCKSTATION_MINI_LICENSE = CC-BY-NC-ND
+
+define DUCKSTATION_MINI_EXTRACT_CMDS
+	mkdir -p $(@D) && \
+	cd $(@D) && \
+	cp $(DL_DIR)/$(DUCKSTATION_MINI_DL_SUBDIR)/$(DUCKSTATION_MINI_SOURCE) $(@D)/
+endef
+
+define DUCKSTATION_MINI_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/duckstation-mini
+	cp -pr $(@D)/$(DUCKSTATION_MINI_SOURCE) $(TARGET_DIR)/usr/duckstation-mini/DuckStation-Mini.AppImage
+	chmod +x $(TARGET_DIR)/usr/duckstation-mini/DuckStation-Mini.AppImage
+
+	# evmap config
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_REGLINUX_PATH)/package/emulators/duckstation-mini/psx.duckstation-mini.keys \
+	    $(TARGET_DIR)/usr/share/evmapy
+endef
+
+$(eval $(generic-package))
