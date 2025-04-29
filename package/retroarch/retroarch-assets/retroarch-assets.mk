f04b9af7755bb0236ffd74863fccb9debb725aca
Romain TISSERAND <romain.tisserand@gmail.com>
2025-04-29 18:53:08 +0200

    RA v1.21.1 bump

--- package/retroarch/retroarch-assets/retroarch-assets.mk
+++ package/retroarch/retroarch-assets/retroarch-assets.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-RETROARCH_ASSETS_VERSION = v1.19.0
+RETROARCH_ASSETS_VERSION = v1.20.0
 RETROARCH_ASSETS_SITE = $(call github,libretro,retroarch-assets,$(RETROARCH_ASSETS_VERSION))
 RETROARCH_ASSETS_LICENSE = GPL
 
 define RETROARCH_ASSETS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/libretro/assets/xmb
-	cp -r $(@D)/menu_widgets $(TARGET_DIR)/usr/share/libretro/assets
 	cp -r $(@D)/ozone $(TARGET_DIR)/usr/share/libretro/assets
 	cp -r $(@D)/rgui $(TARGET_DIR)/usr/share/libretro/assets
 	cp -r $(@D)/glui $(TARGET_DIR)/usr/share/libretro/assets
