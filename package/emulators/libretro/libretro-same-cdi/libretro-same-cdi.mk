0d55b73edb1bcd57af6f51584e1371ce71ea6902
Romain TISSERAND <romain.tisserand@gmail.com>
2025-04-30 22:06:32 +0200

    lr-cdi based on 0.277 (do not PR/merge until tested okay)

--- package/emulators/libretro/libretro-same-cdi/libretro-same-cdi.mk
+++ package/emulators/libretro/libretro-same-cdi/libretro-same-cdi.mk
@@ -3,8 +3,8 @@
 # libretro-same-cdi
 #
 ################################################################################
-# Version: Commits on Apr 23, 2025
-LIBRETRO_SAME_CDI_VERSION = 518a4daf01c20b47a157145e9402e699f4b2f3d5
+# Version: Commits on Apr 30, 2025
+LIBRETRO_SAME_CDI_VERSION = 619a0ccc94f68bc1f18de593dcf1e3b31bbbfdba
 LIBRETRO_SAME_CDI_SITE = $(call github,REG-Linux,libretro-cdi,$(LIBRETRO_SAME_CDI_VERSION))
 LIBRETRO_SAME_CDI_LICENSE = GPL
 
@@ -13,14 +13,14 @@ LIBRETRO_SAME_CDI_DEPENDENCIES = zlib zstd flac rapidjson pugixml sqlite expat j
 LIBRETRO_SAME_CDI_MAX_JOBS = 6
 LIBRETRO_SAME_CDI_JOBS = $(shell if [ $(PARALLEL_JOBS) -gt $(LIBRETRO_SAME_CDI_MAX_JOBS) ]; then echo $(LIBRETRO_SAME_CDI_MAX_JOBS); else echo $(PARALLEL_JOBS); fi)
 
-ifeq ($(BR2_x86_64),y)
-LIBRETRO_SAME_CDI_EXTRA_ARGS += LIBRETRO_CPU=x86_64 PLATFORM=x64
-else ifeq ($(BR2_aarch64),y)
-LIBRETRO_SAME_CDI_EXTRA_ARGS += PTR64=1 LIBRETRO_CPU= PLATFORM=arm64 ARCHITECTURE= NOASM=1
-else ifeq ($(BR2_RISCV_64),y)
-LIBRETRO_SAME_CDI_EXTRA_ARGS += LIBRETRO_CPU=riscv64 PLATFORM=riscv64
-endif
 LIBRETRO_SAME_CDI_EXTRA_ARGS += PTR64=1
+ifeq ($(BR2_x86_64),y)
+LIBRETRO_SAME_CDI_EXTRA_ARGS += LIBRETRO_CPU=x86_64 PLATFORM=x86
+else ifeq ($(BR2_aarch64),y)
+LIBRETRO_SAME_CDI_EXTRA_ARGS += LIBRETRO_CPU=arm64 PLATFORM=arm64 ARCHITECTURE=
+else ifeq ($(BR2_RISCV_64),y)
+LIBRETRO_SAME_CDI_EXTRA_ARGS += LIBRETRO_CPU=riscv64 PLATFORM=riscv64 FORCE_DRC_C_BACKEND=1 ARCHITECTURE=
+endif
 
 define LIBRETRO_SAME_CDI_BUILD_CMDS
 	# First, we need to build genie for host
