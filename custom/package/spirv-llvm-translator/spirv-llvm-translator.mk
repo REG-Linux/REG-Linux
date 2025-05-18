44fd7bac225239a6902cc0da03a4128ba7ccb564
Romain TISSERAND <romain.tisserand@gmail.com>
2025-05-18 13:55:22 +0200

    Fix for panfrost build (+ duckstation gpl + lr-holani) with prebuilt llvm stackA Also remove dead intel-clc package

--- custom/package/spirv-llvm-translator/spirv-llvm-translator.mk
+++ custom/package/spirv-llvm-translator/spirv-llvm-translator.mk
@@ -14,7 +14,7 @@ SPIRV_LLVM_TRANSLATOR_LICENSE_FILES = LICENSE.TXT
 
 # reglinux we need target package for some mesa targets
 SPIRV_LLVM_TRANSLATOR_INSTALL_STAGING = YES
-SPIRV_LLVM_TRANSLATOR_DEPENDENCIES = host-spirv-llvm-translator clang llvm spirv-headers
+SPIRV_LLVM_TRANSLATOR_DEPENDENCIES = host-spirv-llvm-translator llvm clang spirv-headers
 SPIRV_LLVM_TRANSLATOR_CONF_OPTS = \
 	-DLLVM_BUILD_TOOLS=ON \
 	-DLLVM_DIR=$(STAGING_DIR)/lib/cmake/llvm \
@@ -25,7 +25,7 @@ SPIRV_LLVM_TRANSLATOR_CONF_OPTS = \
 $(eval $(cmake-package))
 
 # reglinux define both host and targets
-HOST_SPIRV_LLVM_TRANSLATOR_DEPENDENCIES = host-clang host-llvm host-spirv-headers
+HOST_SPIRV_LLVM_TRANSLATOR_DEPENDENCIES = llvm clang host-spirv-headers
 HOST_SPIRV_LLVM_TRANSLATOR_CONF_OPTS = \
 	-DLLVM_BUILD_TOOLS=ON \
 	-DLLVM_DIR=$(HOST_DIR)/lib/cmake/llvm \
@@ -33,5 +33,25 @@ HOST_SPIRV_LLVM_TRANSLATOR_CONF_OPTS = \
 	-DLLVM_SPIRV_INCLUDE_TESTS=OFF \
 	-DLLVM_EXTERNAL_PROJECTS="SPIRV-Headers" \
 	-DLLVM_EXTERNAL_SPIRV_HEADERS_SOURCE_DIR=$(HOST_DIR)/include
+
+# reglinux fix for prebuilt llvm/clang
+#HOST_SPIRV_LLVM_TRANSLATOR_CONF_OPTS += -DCMAKE_INSTALL_RPATH="$(HOST_DIR)/lib"
+# -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE
+HOST_SPIRV_LLVM_TRANSLATOR_DEPENDENCIES += host-patchelf
+define HOST_SPIRV_LLVM_TRANSLATOR_FIX_RPATH
+    for f in $(HOST_DIR)/bin/llvm-* $(HOST_DIR)/bin/*ll*; do \
+        $(HOST_DIR)/bin/patchelf --set-rpath '$$ORIGIN/../lib' $$f || true; \
+    done
+    $(HOST_DIR)/bin/patchelf --set-rpath '$$ORIGIN/../lib' $(HOST_DIR)/bin/opt || true;
+    $(HOST_DIR)/bin/patchelf --set-rpath '$$ORIGIN/../lib' $(HOST_DIR)/bin/sancov || true;
+    $(HOST_DIR)/bin/patchelf --set-rpath '$$ORIGIN/../lib' $(HOST_DIR)/bin/verify-uselistorder || true;
+    $(HOST_DIR)/bin/patchelf --set-rpath '$$ORIGIN/../lib' $(HOST_DIR)/bin/dsymutil || true;
+    $(HOST_DIR)/bin/patchelf --set-rpath '$$ORIGIN/../lib' $(HOST_DIR)/bin/sanstats || true;
+    $(HOST_DIR)/bin/patchelf --set-rpath '$$ORIGIN/../lib' $(HOST_DIR)/bin/reduce-chunk-list || true;
+    $(HOST_DIR)/bin/patchelf --set-rpath '$$ORIGIN/../lib' $(HOST_DIR)/bin/bugpoint || true;
+endef
+
+HOST_SPIRV_LLVM_TRANSLATOR_POST_INSTALL_HOOKS += HOST_SPIRV_LLVM_TRANSLATOR_FIX_RPATH
+
 $(eval $(host-cmake-package))
 
