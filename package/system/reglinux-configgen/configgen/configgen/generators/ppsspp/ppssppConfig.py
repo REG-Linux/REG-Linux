85778f4a115d17ce5b3e882d975f54c77bf471f7
Romain TISSERAND <romain.tisserand@gmail.com>
2025-05-22 12:28:43 +0200

    Misc fixes : - SDL2 patches to fix video driver order (kms => wayland => X) - Rework Cemu with upstream (might need cherry-pick more patches from old aarch64 fork) - Fix x86_64 kernel patches on 6.12.29 - ET:Legacy riscv fix - Some SpacemiT K1 adjustements

--- package/system/reglinux-configgen/configgen/configgen/generators/ppsspp/ppssppConfig.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/ppsspp/ppssppConfig.py
@@ -146,11 +146,11 @@ def createPPSSPPConfig(iniConfig, system):
     if not iniConfig.has_section("SystemParam"):
         iniConfig.add_section("SystemParam")
 
-    # Forcing Nickname to Batocera or User name
+    # Forcing Nickname to Anonymous or User name
     if system.isOptSet('retroachievements') and system.getOptBoolean('retroachievements') == True and system.isOptSet('retroachievements.username') and system.config.get('retroachievements.username', "") != "":
         iniConfig.set("SystemParam", "NickName", system.config.get('retroachievements.username', ""))
     else:
-        iniConfig.set("SystemParam", "NickName", "Batocera")
+        iniConfig.set("SystemParam", "NickName", "Anonymous")
     # Disable Encrypt Save (permit to exchange save with different machines)
     iniConfig.set("SystemParam", "EncryptSave", "False")
 
