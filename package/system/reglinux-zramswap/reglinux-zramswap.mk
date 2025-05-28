3496e145baae2a09dff7d26539ab5374e43ea201
Juliano Dorigão <jdorigao@gmail.com>
2025-05-28 08:09:28 -0400

    moved and update reglinux-zramswap

--- package/system/reglinux-zramswap/reglinux-zramswap.mk
+++ package/system/reglinux-zramswap/reglinux-zramswap.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-ZRAMSWAP_VERSION = 0.0.1
-ZRAMSWAP_SOURCE =
-ZRAMSWAP_SITE =
+REGLINUX_ZRAMSWAP_VERSION = 0.1.0
+REGLINUX_ZRAMSWAP_SOURCE =
+REGLINUX_ZRAMSWAP_SITE =
 
-define ZRAMSWAP_INSTALL_TARGET_CMDS
+define REGLINUX_ZRAMSWAP_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/reglinux/services
-	$(INSTALL) -Dm755 $(BR2_EXTERNAL_REGLINUX_PATH)/package/batocera/utils/zramswap/zramswap $(TARGET_DIR)/usr/share/reglinux/services/
+	$(INSTALL) -Dm755 $(BR2_EXTERNAL_REGLINUX_PATH)/package/system/reglinux-zramswap/zramswap $(TARGET_DIR)/usr/share/reglinux/services/
 endef
 
 $(eval $(generic-package))
