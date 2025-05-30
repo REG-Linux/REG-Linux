7073aa578ea762f7ea44db4975a432e23a4fd305
Juliano Dorigão <jdorigao@gmail.com>
2025-05-30 13:06:33 -0400

    moved: firmware-rk3588

--- package/firmwares/firmware-rk3588/firmware-rk3588.mk
+++ package/firmwares/firmware-rk3588/firmware-rk3588.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-FIRMWARE_RK3588_VERSION = dc92513ebf859d1213c999e44d4a7bf6a1fb04d7
-FIRMWARE_RK3588_SITE = $(call github,stvhay,rk3588-firmware,$(FIRMWARE_RK3588_VERSION))
-FIRMWARE_RK3588_DEPENDENCIES += alllinuxfirmwares 
+FIRMWARE_RK3588_VERSION = 0.0.1
+FIRMWARE_RK3588_SOURCE =
+FIRMWARE_RK3588_DEPENDENCIES += alllinuxfirmwares
 FIRMWARE_RK3588_DEPENDENCIES += firmware-radxa-rkwifibt
 
+FIRMWARE_RK3588_PATH = $(BR2_EXTERNAL_REGLINUX_PATH)/package/firmwares/firmware-rk3588
 FIRMWARE_RK3588_TARGET_DIR=$(TARGET_DIR)/lib/firmware
 
 define FIRMWARE_RK3588_INSTALL_TARGET_CMDS
-	mkdir -p $(FIRMWARE_RK3588_TARGET_DIR)/rtk_bt
-	cp -v $(@D)/rtl8821cs_* $(FIRMWARE_RK3588_TARGET_DIR)/rtk_bt/
+	cp -prv $(FIRMWARE_RK3588_PATH)/firmware/* $(TARGET_DIR)/lib/firmware/
 endef
 
 $(eval $(generic-package))
