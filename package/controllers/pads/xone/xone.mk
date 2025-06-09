85880170cb6d30b3010184351b3e100c1505328d
Romain TISSERAND <romain.tisserand@gmail.com>
2025-06-09 16:26:01 +0200

    Temptative rk3588 6.16-rc1 based builds

---
+++ package/controllers/pads/xone/xone.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# xone
+#
+################################################################################
+
+# Version v0.3.1
+# medausalix repo dead, use maintained one
+XONE_VERSION = v0.3.1
+XONE_SITE = $(call github,dlundqvist,xone,$(XONE_VERSION))
+XONE_DEPENDENCIES = host-cabextract libusb
+
+define XONE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/install/modprobe.conf $(TARGET_DIR)/etc/modprobe.d/xone-blacklist.conf
+	# copy firmware
+	cp $(BR2_EXTERNAL_REGLINUX_PATH)/package/controllers/pads/xone/FW_ACC_00U.bin \
+	$(TARGET_DIR)/lib/firmware/xow_dongle.bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
