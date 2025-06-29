4ef43f586c35d3a253e5299e908711a59c6efbe0
Romain TISSERAND <romain.tisserand@gmail.com>
2025-06-29 10:48:17 +0200

    sm8550 6.15 rework, missing wifi/usb/sound on odin2

--- board/qualcomm/sm8550/odin2/create-boot-script.sh
+++ board/qualcomm/sm8550/odin2/create-boot-script.sh
@@ -15,6 +15,7 @@ TARGET_DIR=$5
 REGLINUX_BINARIES_DIR=$6
 
 mkdir -p "${REGLINUX_BINARIES_DIR}/boot/boot"     || exit 1
+mkdir -p "${REGLINUX_BINARIES_DIR}/boot/EFI/BOOT" || exit 1
 
 cp "${BINARIES_DIR}/Image"              "${REGLINUX_BINARIES_DIR}/boot/boot/linux"           || exit 1
 cp "${BINARIES_DIR}/rootfs.cpio.lz4"    "${REGLINUX_BINARIES_DIR}/boot/boot/initrd.lz4"      || exit 1
@@ -23,7 +24,16 @@ cp "${BINARIES_DIR}/modules"            "${REGLINUX_BINARIES_DIR}/boot/boot/modu
 cp "${BINARIES_DIR}/firmware"           "${REGLINUX_BINARIES_DIR}/boot/boot/firmware.update" || exit 1
 cp "${BINARIES_DIR}/rescue"             "${REGLINUX_BINARIES_DIR}/boot/boot/rescue.update"   || exit 1
 
-cp "${BINARIES_DIR}/qcs8550-ayn-odin2.dtb"	"${REGLINUX_BINARIES_DIR}/boot/boot/"           || exit 1
-cp -f "${BOARD_DIR}/LinuxLoader.cfg"        "${REGLINUX_BINARIES_DIR}/boot/LinuxLoader.cfg"             || exit 1
+cp "${BINARIES_DIR}/qcs8550-ayn-odin2.dtb"		"${REGLINUX_BINARIES_DIR}/boot/boot/"          	|| exit 1
+cp "${BINARIES_DIR}/qcs8550-ayn-odin2portal.dtb"	"${REGLINUX_BINARIES_DIR}/boot/boot/"          	|| exit 1
+cp "${BINARIES_DIR}/qcs8550-ayn-odin2mini.dtb"		"${REGLINUX_BINARIES_DIR}/boot/boot/"	       	|| exit 1
+
+cp -f "${BOARD_DIR}/boot/LinuxLoader.cfg"        	"${REGLINUX_BINARIES_DIR}/boot/"		|| exit 1
+cp -f "${BOARD_DIR}/boot/boot/u-boot.dtb"        	"${REGLINUX_BINARIES_DIR}/boot/boot/" 		|| exit 1
+cp -f "${BOARD_DIR}/boot/boot/u-boot-nodtb.bin"        	"${REGLINUX_BINARIES_DIR}/boot/boot/"		|| exit 1
+cp -f "${BOARD_DIR}/boot/EFI/BOOT/bootaa64.efi"		"${REGLINUX_BINARIES_DIR}/boot/EFI/BOOT/"	|| exit 1
+cp -f "${BOARD_DIR}/boot/EFI/BOOT/grub.cfg"		"${REGLINUX_BINARIES_DIR}/boot/EFI/BOOT/"	|| exit 1
+cp -f "${BOARD_DIR}/boot/EFI/BOOT/grubenv"		"${REGLINUX_BINARIES_DIR}/boot/EFI/BOOT/"	|| exit 1
+cp -f "${BOARD_DIR}/boot/EFI/BOOT/dejavu-mono.pf2"	"${REGLINUX_BINARIES_DIR}/boot/EFI/BOOT/"	|| exit 1
 
 exit 0
