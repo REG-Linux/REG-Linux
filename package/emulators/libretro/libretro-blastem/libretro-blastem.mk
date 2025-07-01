1389b32f0d0cdfba21bc8c464b0552d4364d5b10
Romain TISSERAND <romain.tisserand@gmail.com>
2025-07-01 15:50:38 +0200

    Some emu bumps/adjusmtents before big MAME bump as separate PR

--- package/emulators/libretro/libretro-blastem/libretro-blastem.mk
+++ package/emulators/libretro/libretro-blastem/libretro-blastem.mk
@@ -12,7 +12,7 @@ LIBRETRO_BLASTEM_LICENSE = Non-commercial
 define LIBRETRO_BLASTEM_BUILD_CMDS
 	cd $(@D) && ./cpu_dsl.py z80.cpu > z80.c
 	cd $(@D) && ./cpu_dsl.py m68k.cpu > m68k.c
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CFLAGS="-Wno-error=incompatible-pointer-types -DHAS_PROC -DHAVE_UNISTD_H -fPIC -DNEW_CORE -DIS_LIB -std=gnu99" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) $(LIBRETRO_BLASTEM_EXTRAOPTS) libblastem.so
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CFLAGS="-Wno-error=incompatible-pointer-types -DHAS_PROC -DHAVE_UNISTD_H -fPIC -DNEW_CORE -DIS_LIB -std=gnu99 -Os -flto=auto" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) $(LIBRETRO_BLASTEM_EXTRAOPTS) libblastem.so
 endef
 
 define LIBRETRO_BLASTEM_INSTALL_TARGET_CMDS
--- package/emulators/pcsx2/pcsx2.mk
+++ package/emulators/pcsx2/pcsx2.mk
@@ -3,8 +3,8 @@
 # pcsx2
 #
 ################################################################################
-# Version v2.3.448 on Jun 23, 2025
-PCSX2_VERSION = v2.3.448
+# Version v2.4.0 on Jun 29, 2025
+PCSX2_VERSION = v2.4.0
 PCSX2_SITE = https://github.com/pcsx2/pcsx2.git
 PCSX2_SITE_METHOD = git
 PCSX2_GIT_SUBMODULES = YES
