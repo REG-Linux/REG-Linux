defc89be5226a30bc041b0a7436e829894dec84c
Romain TISSERAND <romain.tisserand@gmail.com>
2025-07-08 00:10:45 +0200

    24.07 series, lr-geargrafx bump, enable DuckStation UNMODIFIED AppImages

--- package/emulators/libretro/libretro-geargrafx/libretro-geargrafx.mk
+++ package/emulators/libretro/libretro-geargrafx/libretro-geargrafx.mk
@@ -3,8 +3,8 @@
 # libretro-geargrafx
 #
 ################################################################################
-# Version: Release on Jul 6, 2025
-LIBRETRO_GEARGRAFX_VERSION = 1.6.0
+# Version: Release on Jul 7, 2025
+LIBRETRO_GEARGRAFX_VERSION = 1.6.1
 LIBRETRO_GEARGRAFX_SITE = $(call github,drhelius,Geargrafx,$(LIBRETRO_GEARGRAFX_VERSION))
 LIBRETRO_GEARGRAFX_LICENSE = GPLv3
 
