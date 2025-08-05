832af112d3fef32576ebd8cea14bdf99d9ab69ad
Juliano Dorigão <jdorigao@gmail.com>
2025-08-05 14:30:06 -0400

    configgen: amiberry

--- package/system/reglinux-configgen/configgen/configgen/generators/amiberry/amiberryConfig.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/amiberry/amiberryConfig.py
@@ -1,7 +1,7 @@
 from systemFiles import CONF
 
-amiberryRoot = CONF + '/amiberry'
-amiberryRetroarchInputsDir = amiberryRoot + '/conf/retroarch/autoconfig'
-amiberryRetroarchCustom = amiberryRoot + '/conf/retroarch/retroarchcustom.cfg'
-amiberryShare = '/usr/share/amiberry'
-amiberryBin = '/usr/bin/amiberry'
+AMIBERRY_DIR = CONF + '/amiberry'
+AMIBERRY_INPUTS_DIR = AMIBERRY_DIR + '/conf/retroarch/autoconfig'
+AMIBERRY_CONFIG_PATH = AMIBERRY_DIR + '/conf/retroarch/retroarchcustom.cfg'
+AMIBERRY_SHARE_DIR = '/usr/share/amiberry'
+AMIBERRY_BIN_PATH = '/usr/bin/amiberry'
