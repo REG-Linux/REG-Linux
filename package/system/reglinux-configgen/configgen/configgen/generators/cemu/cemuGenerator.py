6f9b0c73f7e4739a1d4f58f995cbc48817e017f5
Juliano Dorigão <jdorigao@gmail.com>
2025-08-06 16:32:19 -0400

    configgen: cemu

--- package/system/reglinux-configgen/configgen/configgen/generators/cemu/cemuGenerator.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/cemu/cemuGenerator.py
@@ -1,9 +1,11 @@
 from generators.Generator import Generator
 from Command import Command
-import os
-import glob
-from . import cemuConfig
-from . import cemuControllers
+from codecs import open
+from xml.dom import minidom
+from os import path, mkdir, linesep
+from glob import iglob, escape
+from .cemuConfig import CEMU_BIN_PATH, CEMU_BIOS_DIR, CEMU_CONFIG_DIR, CEMU_SAVES_DIR, CEMU_CONFIG_PATH, CEMU_PROFILES_DIR, setCemuConfig
+from .cemuControllers import setControllerConfig
 
 from utils.logger import get_logger
 eslog = get_logger(__name__)
@@ -20,17 +22,44 @@ class CemuGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
 
-        # in case of squashfs, the root directory is passed
+        # in case of squashfs/zar, the root directory is passed
         rpxrom = rom
-        paths = list(glob.iglob(os.path.join(glob.escape(rom), '**/code/*.rpx'), recursive=True))
+        paths = list(iglob(path.join(escape(rom), '**/code/*.rpx'), recursive=True))
         if len(paths) >= 1:
             rpxrom = paths[0]
 
+        # bios pack
+        if not path.isdir(CEMU_BIOS_DIR):
+            mkdir(CEMU_BIOS_DIR)
+        if not path.isdir(CEMU_CONFIG_DIR):
+            mkdir(CEMU_CONFIG_DIR)
+        # graphic packs
+        if not path.isdir(CEMU_SAVES_DIR + "/graphicPacks"):
+            mkdir(CEMU_SAVES_DIR + "/graphicPacks")
+        if not path.isdir(CEMU_CONFIG_DIR + "/controllerProfiles"):
+            mkdir(CEMU_CONFIG_DIR + "/controllerProfiles")
+
+        # Config file
+        cemuConfig = minidom.Document()
+        if path.exists(CEMU_CONFIG_PATH):
+            try:
+                cemuConfig = minidom.parse(CEMU_CONFIG_PATH)
+            except:
+                pass # reinit the file
+
         # Create the settings file
-        cemuConfig.CemuConfig(cemuConfig.cemuConfigFile, system)
+        setCemuConfig(cemuConfig, system)
+
+        # Save the config file
+        xml = open(CEMU_CONFIG_PATH, "w")
+
+        # TODO: python 3 - workaround to encode files in utf-8
+        xml = open(CEMU_CONFIG_PATH, "w", "utf-8")
+        dom_string = linesep.join([s for s in cemuConfig.toprettyxml().splitlines() if s.strip()])
+        xml.write(dom_string)
 
         # Set-up the controllers
-        cemuControllers.generateControllerConfig(system, playersControllers, cemuConfig.cemuProfilesDir)
+        setControllerConfig(system, playersControllers, CEMU_PROFILES_DIR)
 
-        commandArray = [cemuConfig.cemuBin, "-f", "--force-no-menubar", "-g", rpxrom]
+        commandArray = [CEMU_BIN_PATH, "-f", "--force-no-menubar", "-g", rpxrom]
         return Command(array=commandArray)
