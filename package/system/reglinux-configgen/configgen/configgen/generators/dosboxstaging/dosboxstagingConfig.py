720789a863c39f537a7e6eeeab0abfa92a4e8328
Juliano Dorigão <jdorigao@gmail.com>
2025-08-06 19:51:25 -0400

    configgen: dosbox-staging

--- package/system/reglinux-configgen/configgen/configgen/generators/dosboxstaging/dosboxstagingConfig.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/dosboxstaging/dosboxstagingConfig.py
@@ -1,5 +1,5 @@
 from systemFiles import CONF
 
-dosboxStagingCustom = CONF + '/dosbox'
-dosboxStagingConfig = dosboxStagingCustom + '/dosbox.conf'
-dosboxStagingBin = '/usr/bin/dosbox-staging'
+DOSBOXSTAGING_CONFIG_DIR = CONF + '/dosbox'
+DOSBOXSTAGING_CONFIG_PATH = DOSBOXSTAGING_CONFIG_DIR + '/dosbox.conf'
+DOSBOXSTAGING_BIN_PATH = '/usr/bin/dosbox-staging'
