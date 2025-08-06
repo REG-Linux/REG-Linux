fb7da02e948cbb5283fd66b4d1717618b463e94a
Juliano Dorigão <jdorigao@gmail.com>
2025-08-05 22:38:42 -0400

    configgen: cannonball

--- package/system/reglinux-configgen/configgen/configgen/generators/cannonball/cannonballGenerator.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/cannonball/cannonballGenerator.py
@@ -1,15 +1,34 @@
 from generators.Generator import Generator
 from Command import Command
-from . import cannonballConfig
+from os import path, makedirs, linesep
+from codecs import open
+from xml.dom import minidom
+from .cannonballConfig import CANNONBALL_BIN_PATH, CANNONBALL_CONFIG_PATH, setCannonballConfig
 
 class CannonballGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
+        if not path.exists(path.dirname(CANNONBALL_CONFIG_PATH)):
+            makedirs(path.dirname(CANNONBALL_CONFIG_PATH))
+
+        # config file
+        cannoballConfig = minidom.Document()
+        if path.exists(CANNONBALL_CONFIG_PATH):
+            try:
+                cannoballConfig = minidom.parse(CANNONBALL_CONFIG_PATH)
+            except:
+                pass # reinit the file
 
         # cannonball config file
-        cannonballConfig.setCannonballConfig(system)
+        setCannonballConfig(cannoballConfig, system)
+
+        # save the config file
+        cannonballXml = open(CANNONBALL_CONFIG_PATH, "w", "utf-8")
+        dom_string = linesep.join([s for s in cannoballConfig.toprettyxml().splitlines() if s.strip()]) # remove ugly empty lines while minicom adds them...
+        cannonballXml.write(dom_string)
+        cannonballXml.close()
 
         # command line
-        commandArray = [cannonballConfig.cannonballBin]
+        commandArray = [CANNONBALL_BIN_PATH]
 
         return Command(array=commandArray)
