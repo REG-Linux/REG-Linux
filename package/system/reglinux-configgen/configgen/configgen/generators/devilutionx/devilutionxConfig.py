1bdc24e3528d707518efa6493754e37b5d1d7f70
Juliano Dorigão <jdorigao@gmail.com>
2025-08-06 18:40:35 -0400

    configgen: devilutionx

---
+++ package/system/reglinux-configgen/configgen/configgen/generators/devilutionx/devilutionxConfig.py
@@ -0,0 +1,6 @@
+from systemFiles import CONF, SAVES, ROMS
+
+DEVILUTIONX_CONFIG_DIR = CONF + "/devilutionx"
+DEVILUTIONX_SAVES_DIR = SAVES + "/devilutionx"
+DEVILUTIONX_ROMS_DIR = ROMS + "/devilutionx"
+DEVILUTIONX_BIN_PATH = "/usr/bin/devilutionx"
--- package/system/reglinux-configgen/configgen/configgen/generators/devilutionx/devilutionxGenerator.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/devilutionx/devilutionxGenerator.py
@@ -1,18 +1,15 @@
 from generators.Generator import Generator
-import os
 from Command import Command
-import controllers as controllersConfig
+from .devilutionxConfig import DEVILUTIONX_CONFIG_DIR, DEVILUTIONX_SAVES_DIR, DEVILUTIONX_ROMS_DIR, DEVILUTIONX_BIN_PATH
+from controllers import generate_sdl_controller_config
 
 class DevilutionXGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
-        configDir = '/userdata/system/configs/devilutionx'
-        saveDir = '/userdata/saves/devilutionx'
-        os.makedirs(configDir, exist_ok=True)
-        os.makedirs(saveDir, exist_ok=True)
 
-        commandArray = ['devilutionx', '--data-dir', '/userdata/roms/devilutionx',
-                        '--config-dir', configDir, '--save-dir', saveDir]
+        commandArray = [DEVILUTIONX_BIN_PATH, '--data-dir', DEVILUTIONX_ROMS_DIR,
+                        '--config-dir', DEVILUTIONX_CONFIG_DIR, '--save-dir', DEVILUTIONX_SAVES_DIR]
+
         if rom.endswith('hellfire.mpq'):
             commandArray.append('--hellfire')
         elif rom.endswith('spawn.mpq'):
@@ -26,5 +23,5 @@ class DevilutionXGenerator(Generator):
         return Command(
                     array=commandArray,
                     env={
-                        'SDL_GAMECONTROLLERCONFIG': controllersConfig.generate_sdl_controller_config(playersControllers)
+                        'SDL_GAMECONTROLLERCONFIG': generate_sdl_controller_config(playersControllers)
                     })
