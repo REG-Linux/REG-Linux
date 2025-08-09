dbb31d1c00264bb2acc83e061d21c7f74f0297aa
Juliano Dorigão <jdorigao@gmail.com>
2025-08-09 10:29:04 -0400

    configgen: duckstation

--- package/system/reglinux-configgen/configgen/configgen/generators/duckstation/duckstationGenerator.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/duckstation/duckstationGenerator.py
@@ -1,7 +1,9 @@
 from generators.Generator import Generator
 from Command import Command
-import os.path
-from . import duckstationConfig
+from configparser import ConfigParser
+from os import path, makedirs
+from .duckstationConfig import DUCKSTATION_NOGUI_PATH, DUCKSTATION_BIN_PATH, DUCKSTATION_CONFIG_PATH, setDuckstationConfig
+from .duckstationControllers import setDuckstationControllers
 
 class DuckstationGenerator(Generator):
     # Duckstation is now QT-only, requires wayland compositor to run
@@ -9,27 +11,39 @@ class DuckstationGenerator(Generator):
         return True
 
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
+        duckstatonConfig = ConfigParser(interpolation=None)
+        duckstatonConfig.optionxform=lambda optionstr: str(optionstr)
 
-        duckstationConfig.readWriteFile(system, playersControllers)
+        if path.exists(DUCKSTATION_CONFIG_PATH):
+            duckstatonConfig.read(DUCKSTATION_CONFIG_PATH)
+
+        setDuckstationConfig(duckstatonConfig, system, playersControllers)
+        setDuckstationControllers(duckstatonConfig, system, metadata, guns, playersControllers)
+
+        # Save config
+        if not path.exists(path.dirname(DUCKSTATION_CONFIG_PATH)):
+            makedirs(path.dirname(DUCKSTATION_CONFIG_PATH))
+        with open(DUCKSTATION_CONFIG_PATH, 'w') as configfile:
+            duckstatonConfig.write(configfile)
 
         # Test if it's a m3u file
-        if os.path.splitext(rom)[1] == ".m3u":
+        if path.splitext(rom)[1] == ".m3u":
             rom = rewriteM3uFullPath(rom)
 
-        if os.path.exists(duckstationConfig.duckstationBin):
-            commandArray = [duckstationConfig.duckstationBin, rom]
+        if path.exists(DUCKSTATION_BIN_PATH):
+            commandArray = [DUCKSTATION_BIN_PATH, rom]
         else:
-            commandArray = ["duckstation-nogui", "-batch", "-fullscreen", "--", rom]
+            commandArray = [DUCKSTATION_NOGUI_PATH, "-batch", "-fullscreen", "--", rom]
 
         return Command(array=commandArray)
 
 def rewriteM3uFullPath(m3u):                                                                    # Rewrite a clean m3u file with valid fullpath
     # get initialm3u
     firstline = open(m3u).readline().rstrip()                                                   # Get first line in m3u
-    initialfirstdisc = "/tmp/" + os.path.splitext(os.path.basename(firstline))[0] + ".m3u"      # Generating a temp path with the first iso filename in m3u
+    initialfirstdisc = "/tmp/" + path.splitext(path.basename(firstline))[0] + ".m3u"      # Generating a temp path with the first iso filename in m3u
 
     # create a temp m3u to bypass Duckstation m3u bad pathfile
-    fulldirname = os.path.dirname(m3u)
+    fulldirname = path.dirname(m3u)
     initialm3u = open(m3u, "r")
 
     with open(initialfirstdisc, 'a') as f1:
