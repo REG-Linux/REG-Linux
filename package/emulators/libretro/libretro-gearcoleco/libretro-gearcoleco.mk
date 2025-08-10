af4923c096b0cd773afda7a7a7479b624e0e3658
Romain TISSERAND <romain.tisserand@gmail.com>
2025-08-10 22:50:28 +0200

    Some emubumps

--- package/emulators/libretro/libretro-gearcoleco/libretro-gearcoleco.mk
+++ package/emulators/libretro/libretro-gearcoleco/libretro-gearcoleco.mk
@@ -3,8 +3,8 @@
 # libretro-gearcoleco
 #
 ################################################################################
-# Version: Release on Jul 8, 2025
-LIBRETRO_GEARCOLECO_VERSION = 1.5.1
+# Version: Release on Aug 10, 2025
+LIBRETRO_GEARCOLECO_VERSION = 1.5.2
 LIBRETRO_GEARCOLECO_SITE = $(call github,drhelius,Gearcoleco,$(LIBRETRO_GEARCOLECO_VERSION))
 LIBRETRO_GEARCOLECO_LICENSE = GPLv3
 
