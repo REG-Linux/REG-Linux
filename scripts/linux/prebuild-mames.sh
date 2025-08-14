535b724189f8f36dec4bf94ce3c18c2c9f97057f
Romain TISSERAND <romain.tisserand@gmail.com>
2025-08-14 13:36:08 +0200

    EDuke32, xenia-canary, lr-clownmdemu bumps + MAME fixes

--- scripts/linux/prebuild-mames.sh
+++ scripts/linux/prebuild-mames.sh
@@ -4,7 +4,8 @@
 # ./scripts/linux/prebuild-mames.sh
 
 # Grab MAME version from source tree
-MAME_VERSION=$(cat package/emulators/mame/mame.mk | grep GroovyMAME | grep Version | cut -d " " -f 4)
+#MAME_VERSION=$(cat package/emulators/mame/mame.mk | grep GroovyMAME | grep Version | cut -d " " -f 4)
+MAME_VERSION=$(cat package/emulators/mame/mame.mk | grep "Version: MAME" | cut -d " " -f 4)
 LRMAME_VERSION=$(cat package/emulators/libretro/libretro-mame/libretro-mame.mk | grep LIBRETRO_MAME_VERSION | grep lrmame | cut -d " " -f 3 | sed s/lrmame//g | sed s/0/0\./)
 
 # Check both version matches, or abort
@@ -34,8 +35,8 @@ for arch in h5 bcm2711 bcm2712 rk3326 rk3399 rk3588 s905gen3 s922x asahi jh7110 
 	# Clean
 	make ${arch}-clean
 	# Build
-	PKG=mame make ${arch}-pkg
-	PKG=libretro-mame make ${arch}-pkg
+	PKG=mame make ${arch}-pkg DEBUG_BUILD=n
+	PKG=libretro-mame make ${arch}-pkg DEBUG_BUILD=n
 	# Package
 	tar cvzf prebuilt/reglinux-mame-${MAME_VERSION}-${arch}.tar.gz output/${arch}/per-package/mame/target/usr/bin/mame/ output/${arch}/per-package/libretro-mame/target/usr/lib/libretro/mame_libretro.so output/${arch}/per-package/mame/target/usr/share/mame/ output/${arch}/per-package/libretro-mame/target/usr/share/lr-mame/
 	# Clean again
