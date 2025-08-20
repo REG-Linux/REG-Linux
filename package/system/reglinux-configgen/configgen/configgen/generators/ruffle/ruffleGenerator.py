21762922460377b02f8092f97d83db9a6a9661d4
Juliano Dorigão <jdorigao@gmail.com>
2025-08-20 10:26:29 -0400

    configgen: ruffle

--- package/system/reglinux-configgen/configgen/configgen/generators/ruffle/ruffleGenerator.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/ruffle/ruffleGenerator.py
@@ -1,12 +1,18 @@
 from generators.Generator import Generator
 from Command import Command
+from controllers import generate_sdl_controller_config
+
+RUFFLE_BIN_PATH = '/usr/bin/ruffle'
 
 class RuffleGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
-        commandArray = ["ruffle", "--fullscreen", rom]
+        commandArray = [RUFFLE_BIN_PATH, "--fullscreen", rom]
         return Command(
-            array=commandArray)
+                    array=commandArray,
+                    env={
+                        'SDL_GAMECONTROLLERCONFIG': generate_sdl_controller_config(playersControllers)
+                    })
 
     def getMouseMode(self, config, rom):
         return True
