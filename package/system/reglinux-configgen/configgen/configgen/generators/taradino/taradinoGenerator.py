f9156b25cef215f0b8756d688a961cd32b0bc6d0
Juliano Dorigão <jdorigao@gmail.com>
2025-08-20 10:46:33 -0400

    configgen: taradino

--- package/system/reglinux-configgen/configgen/configgen/generators/taradino/taradinoGenerator.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/taradino/taradinoGenerator.py
@@ -1,15 +1,21 @@
 from Command import Command
 from generators.Generator import Generator
+from systemFiles import ROMS
+from controllers import generate_sdl_controller_config
+
+ROTT_ROMS_DIR = ROMS + '/rott'
+ROTT_BIN_PATH = '/usr/bin/taradino'
 
 class TaradinoGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
 
-        commandArray = [ "taradino" ]
+        commandArray = [ ROTT_BIN_PATH ]
 
         return Command(
             array=commandArray,
             env={
-                "XDG_DATA_DIRS": "/userdata/roms/rott"
+                "XDG_DATA_DIRS": ROTT_ROMS_DIR,
+                "SDL_GAMECONTROLLERCONFIG": generate_sdl_controller_config(playersControllers)
             }
         )
