ffef0d6af6e43030fe73d5d5d1c712ad5f8a7e52
Juliano Dorigão <jdorigao@gmail.com>
2025-08-22 13:49:03 -0400

    configgen: xemu

--- package/system/reglinux-configgen/configgen/configgen/generators/xemu/xemuGenerator.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/xemu/xemuGenerator.py
@@ -1,27 +1,32 @@
 from generators.Generator import Generator
 from Command import Command
-import os.path
+from os import path, makedirs
 from shutil import copyfile
-from . import xemuConfig
+from controllers import generate_sdl_controller_config
+from .xemuConfig import setXemuConfig, XEMU_BIN_PATH, XEMU_CONFIG_PATH, XEMU_SAVES_DIR
 
 class XemuGenerator(Generator):
 
     # Main entry of the module
     # Configure fba and return a command
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
-        xemuConfig.writeIniFile(system, rom, playersControllers, gameResolution)
+        setXemuConfig(system, rom, playersControllers, gameResolution)
 
         # copy the hdd if it doesn't exist
-        if not os.path.exists("/userdata/saves/xbox/xbox_hdd.qcow2"):
-            if not os.path.exists("/userdata/saves/xbox"):
-                os.makedirs("/userdata/saves/xbox")
-            copyfile("/usr/share/xemu/data/xbox_hdd.qcow2", "/userdata/saves/xbox/xbox_hdd.qcow2")
+        if not path.exists(XEMU_SAVES_DIR + "/xbox_hdd.qcow2"):
+            if not path.exists(XEMU_SAVES_DIR):
+                makedirs(XEMU_SAVES_DIR)
+            copyfile("/usr/share/xemu/data/xbox_hdd.qcow2", XEMU_SAVES_DIR + "/xbox_hdd.qcow2")
 
         # the command to run
-        commandArray = [xemuConfig.xemuBin]
-        commandArray.extend(["-config_path", xemuConfig.xemuConfig])
+        commandArray = [XEMU_BIN_PATH]
+        commandArray.extend(["-config_path", XEMU_CONFIG_PATH])
 
-        return Command(array=commandArray)
+        return Command(
+                    array=commandArray,
+                    env={
+                        'SDL_GAMECONTROLLERCONFIG': generate_sdl_controller_config(playersControllers)
+                    })
 
     def getInGameRatio(self, config, gameResolution, rom):
         if ("xemu_scaling" in config and config["xemu_scaling"] == "stretch") or ("xemu_aspect" in config and config["xemu_aspect"] == "16x9"):
