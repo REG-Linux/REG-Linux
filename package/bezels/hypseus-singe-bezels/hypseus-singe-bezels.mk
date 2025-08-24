13e88b1a7d483e8373987699542470bb8784e62b
Romain TISSERAND <romain.tisserand@gmail.com>
2025-08-24 22:19:00 +0200

    Misc WIP (debloat libclc, bump+debloat shaders, some bumps)

--- package/bezels/hypseus-singe-bezels/hypseus-singe-bezels.mk
+++ package/bezels/hypseus-singe-bezels/hypseus-singe-bezels.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HYPSEUS_SINGE_BEZELS_VERSION = 87a5eaded9cd1a31fcd2d4a36d3e14f976264315
+HYPSEUS_SINGE_BEZELS_VERSION = d64889d4753efdda02e9085e02e4308b74a0730a
 HYPSEUS_SINGE_BEZELS_SITE = \
     $(call github,REG-Linux,hypseus_singe_daphne_bezels,$(HYPSEUS_SINGE_BEZELS_VERSION))
 HYPSEUS_SINGE_BEZELS_LICENSE = LGPL-3.0
