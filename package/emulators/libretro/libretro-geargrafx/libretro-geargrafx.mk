7a71c579128c651e38b0b09c97fd9c9981cae25e
Romain TISSERAND <romain.tisserand@gmail.com>
2025-08-27 16:52:55 +0200

    Bump lr-gear* cores

--- package/emulators/libretro/libretro-geargrafx/libretro-geargrafx.mk
+++ package/emulators/libretro/libretro-geargrafx/libretro-geargrafx.mk
@@ -3,8 +3,8 @@
 # libretro-geargrafx
 #
 ################################################################################
-# Version: Release on Aug 10, 2025
-LIBRETRO_GEARGRAFX_VERSION = 1.6.2
+# Version: Release on Aug 27, 2025
+LIBRETRO_GEARGRAFX_VERSION = 1.6.3
 LIBRETRO_GEARGRAFX_SITE = $(call github,drhelius,Geargrafx,$(LIBRETRO_GEARGRAFX_VERSION))
 LIBRETRO_GEARGRAFX_LICENSE = GPLv3
 
--- package/emulators/libretro/libretro-gearsystem/libretro-gearsystem.mk
+++ package/emulators/libretro/libretro-gearsystem/libretro-gearsystem.mk
@@ -3,8 +3,8 @@
 # libretro-gearsystem
 #
 ################################################################################
-# Version: Release on Aug 10, 2025
-LIBRETRO_GEARSYSTEM_VERSION = 3.8.3
+# Version: Release on Aug 27, 2025
+LIBRETRO_GEARSYSTEM_VERSION = 3.8.4
 LIBRETRO_GEARSYSTEM_SITE = $(call github,drhelius,Gearsystem,$(LIBRETRO_GEARSYSTEM_VERSION))
 LIBRETRO_GEARSYSTEM_LICENSE = GPLv3
 
