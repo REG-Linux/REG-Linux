8d6fd1e88278fbd583914c4167c5b8281a39ef5a
Romain TISSERAND <romain.tisserand@gmail.com>
2025-08-27 16:52:55 +0200

    Bump lr-gear* cores

--- package/emulators/libretro/libretro-gearboy/libretro-gearboy.mk
+++ package/emulators/libretro/libretro-gearboy/libretro-gearboy.mk
@@ -3,8 +3,8 @@
 # libretro-gearboy
 #
 ################################################################################
-# Version: Release on Aug 10, 2025
-LIBRETRO_GEARBOY_VERSION = 3.7.2
+# Version: Release on Aug 27, 2025
+LIBRETRO_GEARBOY_VERSION = 3.7.3
 LIBRETRO_GEARBOY_SITE = $(call github,drhelius,Gearboy,$(LIBRETRO_GEARBOY_VERSION))
 LIBRETRO_GEARBOY_LICENSE = GPLv3
 
