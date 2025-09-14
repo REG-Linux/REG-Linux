37c98e9e968b8577cb9e60925bc4cc791e4e40bd
Juliano Dorigão <jdorigao@gmail.com>
2025-09-13 22:51:02 -0400

    add: Retroid Pocket Gamepad
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/controllers/gamecontrollerdb/gamecontrollerdb.mk
+++ package/controllers/gamecontrollerdb/gamecontrollerdb.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# Version.: Commits on Aug 25, 2025
-GAMECONTROLLERDB_VERSION = 5608aafb948876fea606d3d8f11d7a36eab5e400
+# Version.: Commits on Sep 13, 2025
+GAMECONTROLLERDB_VERSION = ad1750d93ef4ad49fda9b161ac36c00864494f5c
 GAMECONTROLLERDB_SITE = $(call github,REG-Linux,SDL_GameControllerDB,$(GAMECONTROLLERDB_VERSION))
 
 GAMECONTROLLERDB_PATH = $(TARGET_DIR)/usr/share/emulationstation
