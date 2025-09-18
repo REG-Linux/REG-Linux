b48076e796901e83880f1e80d6543cb00d387b26
Romain TISSERAND <romain.tisserand@gmail.com>
2025-09-18 15:57:01 +0200

    WIP Allwinner A527/T527

---
+++ board/allwinner/t527/radxa-cubie-a5e/build-uboot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+HOST_DIR=$1
+BOARD_DIR=$2
+IMAGES_DIR=$3
+
+# Define U-Boot version we use
+UBOOT_VERSION=2025.10-rc4
+
+# Download U-Boot mainline
+wget "https://ftp.denx.de/pub/u-boot/u-boot-${UBOOT_VERSION}.tar.bz2"
+
+# Extract it
+tar xf u-boot-${UBOOT_VERSION}.tar.bz2
+
+# Enter directory
+cd u-boot-${UBOOT_VERSION}
+
+# Apply patches if any
+PATCHES="${BR2_EXTERNAL_REGLINUX_PATH}/board/allwinner/t527/patches/uboot-radxa-cubie-a5e/*.patch"
+for patch in $PATCHES
+do
+echo "Applying patch: $patch"
+patch -p1 < $patch
+done
+
+# Build bootloader
+export BL31="${BINARIES_DIR}/bl31.bin"
+export CROSS_COMPILE="${HOST_DIR}/bin/aarch64-buildroot-linux-gnu-"
+ARCH=aarch64 make radxa-cubie-a5e_defconfig
+ARCH=aarch64 make -j$(nproc)
+
+# Copy generated files
+mkdir -p "${IMAGES_DIR}/reglinux/uboot-radxa-cubie-a5e"
+cp u-boot-sunxi-with-spl.bin ../../uboot-radxa-cubie-a5e/
