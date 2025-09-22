81b194236508476852c5139362a651c5e13e954a
Juliano Dorigão <jdorigao@gmail.com>
2025-09-22 18:30:32 -0400

    es-theme-canvas: update v1.2
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/themes/es-theme-canvas/es-theme-canvas.mk
+++ package/themes/es-theme-canvas/es-theme-canvas.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ES_THEME_CANVAS_VERSION = v1.1
+ES_THEME_CANVAS_VERSION = v1.2
 ES_THEME_CANVAS_SITE = https://github.com/REG-Linux/es-theme-canvas/releases/download/$(ES_THEME_CANVAS_VERSION)
 ES_THEME_CANVAS_SOURCE = canvas-theme-$(ES_THEME_CANVAS_VERSION).tar.gz
 
