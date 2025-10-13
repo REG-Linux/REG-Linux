11d8bf5bde11f3b66f3de3fb3d35290f0fc10bea
Juliano Dorigão <jdorigao@gmail.com>
2025-10-13 19:53:58 -0400

    picade: added dependency (linux)

--- package/cases/picade/picade.mk
+++ package/cases/picade/picade.mk
@@ -6,6 +6,7 @@
 # Version.: Commits on Mar 5, 2020
 PICADE_VERSION = df02844c0cd773af5b908f47eac5fb1f7f361531
 PICADE_SITE = $(call github,pimoroni,picade-hat,$(PICADE_VERSION))
+PICADE_DEPENDENCIES = linux
 
 define PICADE_BUILD_CMDS
     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
