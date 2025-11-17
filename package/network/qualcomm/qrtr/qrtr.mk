a2624354097738838025d0081e51820f0fcc0470
Romain TISSERAND <romain.tisserand@gmail.com>
2025-11-17 12:19:58 +0100

    WIP odin fixes

--- package/network/qualcomm/qrtr/qrtr.mk
+++ package/network/qualcomm/qrtr/qrtr.mk
@@ -9,10 +9,20 @@ QRTR_SITE = $(call github,andersson,qrtr,$(QRTR_VERSION))
 QRTR_LICENSE = BSD-3-Clause license
 QRTR_LICENSE_FILE = LICENSE
 QRTR_DEPENDENCIES = linux-headers
-HOST_QRTR_DEPENDENCIES = linux-headers
+QRTR_INSTALL_STAGING = YES
 
 define QRTR_BUILD_CMDS
     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define QRTR_INSTALL_STAGING_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/qrtr-ns $(STAGING_DIR)/usr/bin
+    $(INSTALL) -D -m 0755 $(@D)/qrtr-cfg $(STAGING_DIR)/usr/bin
+    $(INSTALL) -D -m 0755 $(@D)/qrtr-lookup $(STAGING_DIR)/usr/bin
+    cp $(@D)/libqrtr.so $(STAGING_DIR)/usr/lib
+    cp $(@D)/lib/libqrtr.h $(STAGING_DIR)/usr/include
+    ln -sf libqrtr.so $(STAGING_DIR)/usr/lib/libqrtr.so.1
+    ln -sf libqrtr.so.1 $(STAGING_DIR)/usr/lib/libqrtr.so.1.0
 endef
 
 define QRTR_INSTALL_TARGET_CMDS
@@ -24,13 +34,16 @@ define QRTR_INSTALL_TARGET_CMDS
     ln -sf libqrtr.so.1 $(TARGET_DIR)/usr/lib/libqrtr.so.1.0
 endef
 
-define HOST_QRTR_BUILD_CMDS
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) prefix=$(STAGING_DIR)/usr all
-endef
-
-define HOST_QRTR_INSTALL_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) prefix=$(STAGING_DIR)/usr install
-endef
-
 $(eval $(generic-package))
-$(eval $(host-generic-package))
+
+#HOST_QRTR_DEPENDENCIES = linux-headers
+#
+#define HOST_QRTR_BUILD_CMDS
+#    $(MAKE) -C $(@D) prefix=$(STAGING_DIR)/usr all
+#endef
+#
+#define HOST_QRTR_INSTALL_CMDS
+#    $(MAKE) -C $(@D) prefix=$(STAGING_DIR)/usr install
+#endef
+#
+#$(eval $(host-generic-package))
