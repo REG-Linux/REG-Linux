483ed4f891272bd0c2c575df4ed2e2faa36384c9
cjom <55603581+cjom@users.noreply.github.com>
2025-11-23 15:33:34 +0000

    Bump regmsg (integrity option)

--- package/system/reglinux-msg/reglinux-msg.mk
+++ package/system/reglinux-msg/reglinux-msg.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# v0.0.6 - Nov 22, 2025
-REGLINUX_MSG_VERSION = 1c8828b93c479791e9c5a52c46dd32bfbdd048e6
+# v0.0.6 - Nov 23, 2025
+REGLINUX_MSG_VERSION = b30132e5a4efa6c01ae480a21f1a7a0d4fdd011f
 REGLINUX_MSG_TOKEN = $(shell cat /build/gh_token)
 REGLINUX_MSG_SITE = https://$(REGLINUX_MSG_TOKEN)@github.com/REG-Linux/regmsg
 REGLINUX_MSG_SITE_METHOD = git
@@ -15,6 +15,7 @@ REGLINUX_MSG_DEPENDENCIES += libdrm
 
 RUSTC_TARGET_PROFILE = $(if $(BR2_ENABLE_DEBUG),,release)
 REGLINUX_MSG_LOCATION = target/$(RUSTC_TARGET_NAME)/$(RUSTC_TARGET_PROFILE)
+REGLINUX_MSG_CARGO_BUILD_OPTS += --features integrity
 
 define REGLINUX_MSG_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/$(REGLINUX_MSG_LOCATION)/regmsg			$(TARGET_DIR)/usr/bin/
