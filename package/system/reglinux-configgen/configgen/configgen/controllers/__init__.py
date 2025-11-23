5dc1b476b1312ee92addfcdaa3427e0266e343e5
Juliano Dorigão <jdorigao@gmail.com>
2025-11-23 11:43:13 -0400

    Add controller functionality
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/system/reglinux-configgen/configgen/configgen/controllers/__init__.py
+++ package/system/reglinux-configgen/configgen/configgen/controllers/__init__.py
@@ -1,4 +1,7 @@
 # Controller functionality
+from .controller import generate_sdl_controller_config
+from .controllerdb import load_controller_config
 from .guns import getGuns, gunsNeedCrosses, gunsBordersSizeName
 from .metadata import getGamesMetaData
 from .devices import getDevicesInformation
+from .evmapy import Evmapy
