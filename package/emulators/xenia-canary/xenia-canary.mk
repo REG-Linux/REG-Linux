2647ddedbb8034fdd9f409eb5afb3dcea96098c7
Romain TISSERAND <romain.tisserand@gmail.com>
2025-11-27 19:18:20 +0100

    WIP WIP WIP do not use

--- package/emulators/xenia-canary/xenia-canary.mk
+++ package/emulators/xenia-canary/xenia-canary.mk
@@ -4,7 +4,10 @@
 #
 ################################################################################
 # Version: Commits on Aug 12, 2025
-XENIA_CANARY_VERSION = 7d379952f19bded6931f821fad7df29166ec2cc3
+#XENIA_CANARY_VERSION = 7d379952f19bded6931f821fad7df29166ec2cc3
+#XENIA_CANARY_SITE = https://github.com/xenia-canary/xenia-canary
+# Version: Custom
+XENIA_CANARY_VERSION = 3911cc33018bcd96284ffa43285aa387d409e0db
 XENIA_CANARY_SITE = https://github.com/xenia-canary/xenia-canary
 XENIA_CANARY_SITE_METHOD = git
 XENIA_CANARY_GIT_SUBMODULES = YES
@@ -13,19 +16,34 @@ XENIA_CANARY_LICENSE_FILE = LICENSE
 
 XENIA_CANARY_DEPENDENCIES = python-toml llvm clang libgtk3 sdl2 host-sdl2
 
+# Remove linker flags wrongly passed to clang compile stage
+XENIA_CANARY_CONF_ENV += CFLAGS="$(filter-out -Wl,-z,max-page-size=4096 -Wl,-z,common-page-size=4096,$(CFLAGS))"
+XENIA_CANARY_CONF_ENV += CXXFLAGS="$(filter-out -Wl,-z,max-page-size=4096 -Wl,-z,common-page-size=4096,$(CXXFLAGS))"
+
+# Skip fatal warnings
+XENIA_CANARY_CONF_OPTS += -DCMAKE_CXX_FLAGS="-Wno-unused-command-line-argument"
+
+# Fix C++ standard library visibility for Clang cross ARM64
+
+# Ensure linker flags go to the linker only
+XENIA_CANARY_CONF_ENV += LDFLAGS="$(LDFLAGS) -Wl,-z,max-page-size=4096 -Wl,-z,common-page-size=4096"
+
 define XENIA_CANARY_CONFIGURE_CMDS
 	mkdir -p $(@D) && cd $(@D) && \
+	XENIA_ARCHITECTURE=$(BR2_ARCH) \
 	PKGCONFIG="$(HOST_DIR)/bin/pkg-config" \
 	PKGCONFIG_CONFIG=$(STAGING_DIR)/usr/lib/pkgconfig \
 	SYSROOT="$(STAGING_DIR)" \
 	SDL2CONFIG="$(HOST_DIR)/bin/sdl2-config" \
 	CMAKE_MAKE_PROGRAM="$(HOST_DIR)/bin/ninja" \
-	CC="$(HOST_DIR)/bin/clang" \
+	PREMAKE_NO_VERSION=1 \
 	./xenia-build.py premake
+#	CC="$(HOST_DIR)/bin/clang"
 endef
 
 define XENIA_CANARY_BUILD_CMDS
 	mkdir -p $(@D) && cd $(@D) && \
+	XENIA_ARCHITECTURE=$(BR2_ARCH) \
 	PKGCONFIG="$(HOST_DIR)/bin/pkg-config" \
 	PKGCONFIG_CONFIG=$(STAGING_DIR)/usr/lib/pkgconfig \
 	SYSROOT="$(STAGING_DIR)" \
@@ -33,6 +51,8 @@ define XENIA_CANARY_BUILD_CMDS
 	NINJA="$(HOST_DIR)/bin/ninja" \
 	CC="$(HOST_DIR)/bin/clang" \
 	CXX="$(HOST_DIR)/bin/clang++" \
+	CXXFLAGS="$(CXXFLAGS)  -I$(STAGING_DIR)/usr/include/c++/14.3.0 -I$(STAGING_DIR)/usr/include/c++/14.3.0/aarch64-buildroot-linux-gnu -stdlib=libstdc++ --gcc-toolchain=$(HOST_DIR)/aarch64-buildroot-linux-gnu --sysroot=$(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot" \
+	LDFLAGS="--target=aarch64-linux-gnu --gcc-toolchain=$(HOST_DIR)/aarch64-buildroot-linux-gnu --sysroot=$(HOST_DIR)/aarch64-buildroot-linux-gnu/sysroot -stdlib=libstdc++ $(LDFLAGS)" \
 	./xenia-build.py build --config release
 endef
 
