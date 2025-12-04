16db75edb510db2a32b5532d6fc7afd0c2c86798
Romain TISSERAND <romain.tisserand@gmail.com>
2025-12-04 02:28:54 +0100

    WIP pdfium prebuilt

---
+++ package/libraries/pdfium/pdfium.mk
@@ -0,0 +1,42 @@
+################################################################################
+# PDFium prebuilt binary distribution (bblanchon/pdfium-binaries)
+################################################################################
+
+PDFIUM_VERSION = 7557
+PDFIUM_SITE = https://github.com/bblanchon/pdfium-binaries/releases/download/chromium/$(PDFIUM_VERSION)
+PDFIUM_LICENSE = MIT
+PDFIUM_LICENSE_FILES = LICENSE
+
+PDFIUM_LIBC_SUFFIX =
+PDFIUM_ARCH =
+
+ifeq ($(BR2_arm),y)
+PDFIUM_ARCH = arm
+else ifeq ($(BR2_aarch64),y)
+PDFIUM_ARCH = arm64
+else ifeq ($(BR2_x86_64),y)
+PDFIUM_ARCH = x64
+else
+$(error BR2_PACKAGE_PDFIUM only supports arm, aarch64 or x86_64 targets)
+endif
+
+ifeq ($(BR2_TOOLCHAIN_USES_MUSL),y)
+PDFIUM_LIBC_SUFFIX = -musl
+else ifeq ($(BR2_TOOLCHAIN_USES_GLIBC),y)
+PDFIUM_LIBC_SUFFIX =
+else
+$(error BR2_PACKAGE_PDFIUM requires a glibc or musl toolchain)
+endif
+
+PDFIUM_SOURCE = pdfium-linux$(PDFIUM_LIBC_SUFFIX)-$(PDFIUM_ARCH).tgz
+PDFIUM_SITE_METHOD = https
+
+define PDFIUM_INSTALL_TO_ROOT
+	$(INSTALL) -d $(1)/usr/lib
+	cp $(PDFIUM_DIR)/libpdfium.so $(1)/usr/lib/
+endef
+
+PDFIUM_INSTALL_STAGING_CMDS = $(call PDFIUM_INSTALL_TO_ROOT,$(STAGING_DIR))
+PDFIUM_INSTALL_TARGET_CMDS = $(call PDFIUM_INSTALL_TO_ROOT,$(TARGET_DIR))
+
+$(eval $(generic-package))
