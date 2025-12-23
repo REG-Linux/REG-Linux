ba4abb5cee68759f37f3e7ec5a9da4394429c435
Juliano Dorigão <jdorigao@gmail.com>
2025-12-23 08:33:38 -0400

    Refactor openbor controller setup
    This commit refactors the `setupControllers` function for OpenBOR by
    introducing clear mappings for button and axis controls. This improves
    readability and makes the code more maintainable. Additionally, it
    handles the removal of old configuration keys for unused player slots
    more efficiently.
    
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/system/reglinux-configgen/configgen/configgen/generators/openbor/openborControllers.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/openbor/openborControllers.py
@@ -69,107 +69,55 @@ def JoystickValue(key, pad, joy_max_inputs, new_axis_vals, invertAxis=False):
 
 
 def setupControllers(config, playersControllers, joy_max_inputs, new_axis_vals):
-    idx = 0
-    for playercontroller, pad in sorted(playersControllers.items()):
-        config.save(
-            "keys." + str(idx) + ".0",
-            JoystickValue("up", pad, joy_max_inputs, new_axis_vals),
-        )  # MOVEUP
-        config.save(
-            "keys." + str(idx) + ".1",
-            JoystickValue("down", pad, joy_max_inputs, new_axis_vals),
-        )  # MOVEDOWN
-        config.save(
-            "keys." + str(idx) + ".2",
-            JoystickValue("left", pad, joy_max_inputs, new_axis_vals),
-        )  # MOVELEFT
-        config.save(
-            "keys." + str(idx) + ".3",
-            JoystickValue("right", pad, joy_max_inputs, new_axis_vals),
-        )  # MOVERIGHT
-        config.save(
-            "keys." + str(idx) + ".4",
-            JoystickValue("b", pad, joy_max_inputs, new_axis_vals),
-        )  # ATTACK
-        config.save(
-            "keys." + str(idx) + ".5",
-            JoystickValue("x", pad, joy_max_inputs, new_axis_vals),
-        )  # ATTACK2
-        config.save(
-            "keys." + str(idx) + ".6",
-            JoystickValue("pageup", pad, joy_max_inputs, new_axis_vals),
-        )  # ATTACK3
-        config.save(
-            "keys." + str(idx) + ".7",
-            JoystickValue("pagedown", pad, joy_max_inputs, new_axis_vals),
-        )  # ATTACK4
-        config.save(
-            "keys." + str(idx) + ".8",
-            JoystickValue("a", pad, joy_max_inputs, new_axis_vals),
-        )  # JUMP
-        config.save(
-            "keys." + str(idx) + ".9",
-            JoystickValue("y", pad, joy_max_inputs, new_axis_vals),
-        )  # SPECIAL
-        config.save(
-            "keys." + str(idx) + ".10",
-            JoystickValue("start", pad, joy_max_inputs, new_axis_vals),
-        )  # START
-        config.save(
-            "keys." + str(idx) + ".11",
-            JoystickValue("l2", pad, joy_max_inputs, new_axis_vals),
-        )  # SCREENSHOT
+    # Button control mappings for better readability and maintenance
+    CONTROL_MAPPINGS = [
+        ("dpup", "MOVEUP"),
+        ("dpdown", "MOVEDOWN"),
+        ("dpleft", "MOVELEFT"),
+        ("dpright", "MOVERIGHT"),
+        ("b", "ATTACK"),
+        ("x", "ATTACK2"),
+        ("leftshoulder", "ATTACK3"),
+        ("rightshoulder", "ATTACK4"),
+        ("a", "JUMP"),
+        ("y", "SPECIAL"),
+        ("start", "START"),
+        ("triggerleft", "SCREENSHOT"),
+    ]
 
-        # hotkey
-        if idx == 0:
+    # Analog axis mappings (axis_name, inverted, description)
+    AXIS_MAPPINGS = [
+        ("lefty", False, "axis up"),
+        ("lefty", True, "axis down"),
+        ("leftx", False, "axis left"),
+        ("leftx", True, "axis right"),
+    ]
+
+    MAX_PLAYERS = 5
+    KEYS_PER_PLAYER = 17  # 0-16
+
+    # Configure controls for each connected player
+    for idx, (playercontroller, pad) in enumerate(sorted(playersControllers.items())):
+        key_idx = 0
+
+        # Configure button controls
+        for button, description in CONTROL_MAPPINGS:
             config.save(
-                "keys." + str(idx) + ".12",
-                JoystickValue("hotkey", pad, joy_max_inputs, new_axis_vals),
-            )  # ESC
-        else:
-            config.save("keys." + str(idx) + ".12", "0")  # ESC
+                f"keys.{idx}.{key_idx}",
+                JoystickValue(button, pad, joy_max_inputs, new_axis_vals),
+            )
+            key_idx += 1
 
-        # axis
-        config.save(
-            "keys." + str(idx) + ".13",
-            JoystickValue("joystick1up", pad, joy_max_inputs, new_axis_vals),
-        )  # axis up
-        config.save(
-            "keys." + str(idx) + ".14",
-            JoystickValue("joystick1up", pad, joy_max_inputs, new_axis_vals, True),
-        )  # axis down
-        config.save(
-            "keys." + str(idx) + ".15",
-            JoystickValue("joystick1left", pad, joy_max_inputs, new_axis_vals),
-        )  # axis left
-        config.save(
-            "keys." + str(idx) + ".16",
-            JoystickValue("joystick1left", pad, joy_max_inputs, new_axis_vals, True),
-        )  # axis right
+        # Configure analog axis controls
+        for axis, invert, description in AXIS_MAPPINGS:
+            config.save(
+                f"keys.{idx}.{key_idx}",
+                JoystickValue(axis, pad, joy_max_inputs, new_axis_vals, invert),
+            )
+            key_idx += 1
 
-        # next one
-        idx += 1
-
-    # erase old values in case a pad is reused in an other position (so it is not used twice)
-    for idx in range(len(playersControllers), 5):
-        config.remove("keys." + str(idx) + ".0")
-        config.remove("keys." + str(idx) + ".1")
-        config.remove("keys." + str(idx) + ".2")
-        config.remove("keys." + str(idx) + ".3")
-        config.remove("keys." + str(idx) + ".4")
-        config.remove("keys." + str(idx) + ".5")
-        config.remove("keys." + str(idx) + ".6")
-        config.remove("keys." + str(idx) + ".7")
-        config.remove("keys." + str(idx) + ".8")
-        config.remove("keys." + str(idx) + ".9")
-        config.remove("keys." + str(idx) + ".10")
-        config.remove("keys." + str(idx) + ".11")
-
-        # hotkey
-        if idx != 0:
-            config.remove("keys." + str(idx) + ".12")
-
-        config.remove("keys." + str(idx) + ".13")
-        config.remove("keys." + str(idx) + ".14")
-        config.remove("keys." + str(idx) + ".15")
-        config.remove("keys." + str(idx) + ".16")
+    # Erase old values for unused player slots
+    # (prevents a controller from being used in two different positions)
+    for idx in range(len(playersControllers), MAX_PLAYERS):
+        for key_idx in range(KEYS_PER_PLAYER):
+            config.remove(f"keys.{idx}.{key_idx}")
