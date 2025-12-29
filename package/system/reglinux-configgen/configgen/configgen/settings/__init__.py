ba0f54e6040df2e3b545b4614c78b157c19bf7c2
Juliano Dorigão <jdorigao@gmail.com>
2025-12-29 13:32:24 -0400

    xemu: Add TOML support to settings manager
    Introduces TOMLSettings class and adds tomli and tomli-w as dependencies
    to support TOML configuration files. This enhances the configuration
    manager library's flexibility by allowing it to handle multiple
    configuration file formats.
    
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/system/reglinux-configgen/configgen/configgen/settings/__init__.py
+++ package/system/reglinux-configgen/configgen/configgen/settings/__init__.py
@@ -1,4 +1,5 @@
 from .json_settings import JSONSettings
+from .toml_settings import TOMLSettings
 from .unix_settings import UnixSettings
 
-__all__ = ["JSONSettings", "UnixSettings"]
+__all__ = ["JSONSettings", "TOMLSettings", "UnixSettings"]
