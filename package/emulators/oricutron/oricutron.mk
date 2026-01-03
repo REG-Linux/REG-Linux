c682fe13196250b001c85501ce0923c9f3d3dd72
Romain TISSERAND <romain.tisserand@gmail.com>
2026-01-03 17:30:19 +0100

    Bumps 2026/01/03 + wip max

--- package/emulators/oricutron/oricutron.mk
+++ package/emulators/oricutron/oricutron.mk
@@ -3,8 +3,8 @@
 # Oricutron - Oric computers emulator
 #
 ################################################################################
-# Version.: Release on Dec 22, 2025
-ORICUTRON_VERSION = V1_2_11
+# Version.: Release on Dec 31, 2025
+ORICUTRON_VERSION = V1_2_12
 ORICUTRON_SITE = https://github.com/pete-gordon/oricutron
 ORICUTRON_SITE_METHOD=git
 ORICUTRON_LICENSE = GPLv3
