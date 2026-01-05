fe236fd3e3be651b6c0a6934ad57b9bf95fc7fd6
Romain TISSERAND <romain.tisserand@gmail.com>
2026-01-05 05:34:04 +0100

    RPCS3 0.0.39 bump (unsure about LLVM version, one savestate patch missing)
    Signed-off-by: Romain TISSERAND <romain.tisserand@gmail.com>

--- package/emulators/rpcs3/rpcs3.mk
+++ package/emulators/rpcs3/rpcs3.mk
@@ -3,8 +3,8 @@
 # rpcs3
 #
 ################################################################################
-# Version v0.0.38 - Commits on Oct 1, 2025
-RPCS3_VERSION = v0.0.38
+# Version v0.0.39 - Commits on Jan 1, 2026
+RPCS3_VERSION = v0.0.39
 RPCS3_SITE = https://github.com/RPCS3/rpcs3.git
 RPCS3_SITE_METHOD=git
 RPCS3_GIT_SUBMODULES=YES
