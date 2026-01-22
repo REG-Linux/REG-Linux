7e13e6bf8a5773d0ac76de7c7cbb455c9fb4a281
Romain TISSERAND <romain.tisserand@gmail.com>
2026-01-22 08:41:38 +0100

    Solarus-engine to 2.0.3

---
+++ package/engines/solarus-engine/solarus-engine.mk
@@ -0,0 +1,36 @@
+################################################################################
+#
+# solarus-engine
+#
+################################################################################
+# Version: Release on Jan 21, 2026
+SOLARUS_ENGINE_VERSION = v2.0.3
+SOLARUS_ENGINE_SITE = https://gitlab.com/solarus-games/solarus
+SOLARUS_ENGINE_SITE_METHOD=git
+
+SOLARUS_ENGINE_LICENSE = GPL-3.0 (code), CC-BY-SA-4.0 (Solarus logos and icons), \
+	CC-BY-SA-3.0 (GUI icons)
+SOLARUS_ENGINE_LICENSE_FILES = license.txt
+
+# Install libsolarus.so
+SOLARUS_ENGINE_INSTALL_STAGING = YES
+
+SOLARUS_ENGINE_DEPENDENCIES += reglinux-luajit glm libmodplug libogg libvorbis
+SOLARUS_ENGINE_DEPENDENCIES += openal physfs sdl2 sdl2_image sdl2_ttf
+
+# Disable launcher GUI (requires Qt5)
+SOLARUS_ENGINE_CONF_OPTS = \
+	-DSOLARUS_GUI=OFF \
+	-DSOLARUS_TESTS=OFF
+
+ifeq ($(BR2_PACKAGE_HAS_LIBGLES),y)
+SOLARUS_ENGINE_DEPENDENCIES += libgles
+SOLARUS_ENGINE_CONF_OPTS += -DSOLARUS_GL_ES=ON
+endif
+
+SOLARUS_ENGINE_CONF_OPTS += -DSOLARUS_BASE_WRITE_DIR=/userdata/saves
+SOLARUS_ENGINE_CONF_OPTS += -DSOLARUS_WRITE_DIR=solarus
+
+SOLARUS_ENGINE_CONF_OPTS += -DSOLARUS_USE_LUAJIT=ON
+
+$(eval $(cmake-package))
