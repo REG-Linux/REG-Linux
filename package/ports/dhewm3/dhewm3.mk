bae4eca61b6dcd7012f990dec8129fc1037e4fc2
Romain TISSERAND <romain.tisserand@gmail.com>
2026-01-27 14:51:59 +0100

    Dhewm3 to 1.5.5_RC2 (and SDL3)

---
+++ package/ports/dhewm3/dhewm3.mk
@@ -0,0 +1,32 @@
+################################################################################
+#
+# dhewm3
+#
+################################################################################
+
+DHEWM3_VERSION = 1.5.5_RC2
+DHEWM3_SITE = https://github.com/dhewm/dhewm3
+DHEWM3_LICENSE = GPLv3
+DHEWM3_LICENSE_FILES = COPYING.txt
+DHEWM3_SITE_METHOD=git
+DHEWM3_DEPENDENCIES = host-libjpeg libcurl libogg libvorbis openal sdl3 zlib
+DHEWM3_SUPPORTS_IN_SOURCE_BUILD = NO
+
+DHEWM3_SUBDIR = neo
+
+DHEWM3_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+DHEWM3_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+DHEWM3_CONF_OPTS += -DBUILD_STATIC_LIBS=ON
+DHEWM3_CONF_OPTS += -DENABLE_TEST=OFF
+DHEWM3_CONF_OPTS += -DSDL2=OFF
+DHEWM3_CONF_OPTS += -DSDL3=ON
+
+define DHEWM3_EVMAPY
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp -prn $(BR2_EXTERNAL_REGLINUX_PATH)/package/ports/dhewm3/doom3.dhewm3.keys \
+		$(TARGET_DIR)/usr/share/evmapy
+endef
+
+DHEWM3_POST_INSTALL_TARGET_HOOKS = DHEWM3_EVMAPY
+
+$(eval $(cmake-package))
